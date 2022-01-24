ba515313b1ddce0de4604e5b65c09daece0da903
Adam Shih <shihadam1@gmail.com>
2022-01-24 13:00:42 -0800

    Removed findByDonorId. Similar functionality will be using filtering

--- api/donations/donations.service.ts
+++ api/donations/donations.service.ts
@@ -9,8 +9,3 @@ export const find = async (id: string) => {
   const myQuery = `SELECT * FROM donations WHERE donationid = ${id}`;
   return pool.query(myQuery);
 };
-
-export const findByDonorId = async (donorId: string) => {
-  const myQuery = `SELECT * FROM donations WHERE donorid = ${donorId}`;
-  return pool.query(myQuery);
-};
