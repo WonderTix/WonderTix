66d4405f737dea579a027da3d64444ce29ce0790
Adam <shihadam1@gmail.com>
2022-02-15 11:43:41 -0800

    Added multiple operators

---
+++ server/api/util/ops.ts
@@ -0,0 +1,38 @@
+// api/util/ops.ts
+
+/**
+ * Simple operators.
+ * Operators that funciton similar to `A OP B`
+ *
+ * Example: `id = 3'
+ *
+ */
+const simple_ops: any = {
+  $eq: '=',
+  $lt: '<',
+  $lte: '<=',
+  $gt: '>',
+  $gte: '>='
+};
+
+/**
+ * Convert opcode and operands into query clause.
+ *
+ * @param op
+ * @param a
+ * @param b
+ */
+export function useOp(op: string, a: string, b: any) {
+  if (simple_ops[op]) {
+    return `${a} ${simple_ops[op]} ${b} `;
+  }
+
+  switch (op) {
+    case '$startsWith':
+      return `${a} LIKE \'${b}%\' `;
+    case '$endsWith':
+      return `${a} LIKE \'%${b}\' `;
+  }
+
+  throw 'Operator not found';
+}
