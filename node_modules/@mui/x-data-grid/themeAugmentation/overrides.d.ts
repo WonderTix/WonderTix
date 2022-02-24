1319941288c442b39d3b05c62d3d2cbaf56533c0
Shane McBroom <mcbroom@pdx.edu>
2022-02-23 21:32:47 -0800

    Refactored to use DataGrid.

--- node_modules/@mui/x-data-grid/themeAugmentation/overrides.d.ts
+++
@@ -1,8 +0,0 @@
-import { GridClassKey } from '../../../_modules_/grid/gridClasses';
-export interface DataGridComponentNameToClassKey {
-    MuiDataGrid: GridClassKey;
-}
-declare module '@mui/material/styles/overrides' {
-    interface ComponentNameToClassKey extends DataGridComponentNameToClassKey {
-    }
-}
