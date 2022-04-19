41c46411bc6984400dea7408bf8a56953fb8aa9e
Josh Kasten <jokasten@pdx.edu>
2022-04-18 21:42:34 -0700

    parameterized queries, added task_notes, and rest of CRUD

--- server/api/tasks/tasks.router.ts
+++ server/api/tasks/tasks.router.ts
@@ -1,7 +1,7 @@
 // api/tasks/tasks.router.ts
 
 import { Router } from 'express';
-import { findAll, find } from './tasks.service';
+import { findAll, find, create, remove, update } from './tasks.service';
 
 export const tasksRouter = Router();
 
@@ -20,6 +20,36 @@ tasksRouter.get('/:id', async (req, res) => {
   try {
     const task = await find(req.params.id);
     res.status(200).send(task.rows);
+  } catch (err: any) {
+    res.status(500).send(err.message);
+  }
+});
+
+// POST /api/tasks
+tasksRouter.post('/', async (req, res) => {
+  try {
+    const newTask = await create(req.body);
+    res.status(201).send(newTask.rows);
+  } catch (err: any) {
+    res.status(500).send(err.message);
+  }
+});
+
+// DELETE /api/tasks/:id
+tasksRouter.delete('/:id', async (req, res) => {
+  try {
+    await remove(req.params.id);
+    res.sendStatus(204);
+  } catch (err: any) {
+    res.status(500).send(err.message);
+  }
+});
+
+// PUT /api/tasks/:id
+tasksRouter.put('/:id', async (req, res) => {
+  try {
+    const updatedTask = await update(req);
+    res.sendStatus(204).send(updatedTask.rows);
   } catch (err: any) {
     res.status(500).send(err.message);
   }
