6bf4dcdba5f15026dac68306c20338f387f01f02
Josh Kasten <jokasten@pdx.edu>
2022-05-16 16:05:38 -0700

    added graphql server. wont generate resolvers

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,11 @@
 // server.ts
 
+import 'reflect-metadata';
+import * as tq from 'type-graphql';
+import { ApolloServer } from 'apollo-server';
+import { context } from './context';
+import { resolvers } from "@generated/type-graphql"
+
 import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
@@ -14,6 +20,17 @@ import { task_notesRouter } from './api/task_notes/task_notes.router';
 import { savedReportsRouter } from './api/saved_reports/saved_reports.router';
 
 dotenv.config({ path: path.join(__dirname, '../.env') });
+
+const gql_app = async () => {
+  const schema = await tq.buildSchema({
+    resolvers, 
+  })
+  
+  
+  new ApolloServer({ schema, context: context }).listen({ port: 4000 }, () =>
+    console.log('graphql server ready at: http://localhost:4000')
+  )
+}
 
 const app = express();
 const port = parseInt(process.env.PORT || '8000');
@@ -44,6 +61,7 @@ app.use('/api/saved_reports', savedReportsRouter);
 app.get('/', (req, res) => res.send('Hello World.'));
 
 /* Server Activation */
+gql_app()
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://${hostname}:${port}`);
