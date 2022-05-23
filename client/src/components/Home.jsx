d0a81ceb16f3b8208d87a7ca212f8cef5645714c
Mazin Ashfaq <mzash00@gmail.com>
2022-05-22 19:13:41 -0700

    Changed token auth mode to POST, getting 401 error again

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import LogoutButton from "./Authentication/logout-button";
 
 export default function Home() {
   return (
     <>
       <h3>Welcome to the home page</h3>
+      <LogoutButton></LogoutButton>
     </>
   );
 }
