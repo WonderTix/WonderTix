8195278b2b18966ca7217829eab3182231fd961b
Sam Rind <orind@pdx.edu>
2022-05-24 19:04:23 -0700

    Added notes functionality for edit page

--- client/src/components/Tasks/EditTask.jsx
+++ client/src/components/Tasks/EditTask.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import TaskForm from "./TaskForm"
+import React, { useEffect } from 'react'
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper"; 
+import { Container } from '@material-ui/core';
+import TaskForm from "./TaskForm";
+import Note from "./Note.jsx"; 
 
-export default function Tasks(){
+
+export default function EditTask() {
     return(
-        <div>
-            "This is new Edit" <TaskForm />
+        <div className="parent-component">
+            <div className="create-component"><TaskForm/></div>
+            <div className="Note-component"><Note/></div> 
+            <div className="filter-component">this is filter</div>
         </div>
     )
 }
