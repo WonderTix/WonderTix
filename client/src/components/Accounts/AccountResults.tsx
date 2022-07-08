e4b56812c6e26cd5d7c1ccb5b009384f3e0e75b5
Al1999 <aya9@pdx.edu>
2022-07-08 11:09:35 -0700

    New changes

--- client/src/components/Accounts/AccountResults.tsx
+++ client/src/components/Accounts/AccountResults.tsx
@@ -1,34 +1,29 @@
 import React from 'react';
-import {Button, Paper, Typography} from '@mui/material';
 
 const AccountResults = ({
   data,
 }: {
   data: any,
 }): React.ReactElement => {
-  if (!data) return <Typography></Typography>;
+  if (!data) return <div></div>;
 
   const {username, id, is_superadmin: isSuperadmin} = data;
 
   return (
-    <Paper
-      elevation={6}
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        mt: 2,
-        p: 4,
-        width: 500,
-      }}
+    <div className='flex flex-col mt-2 p-5 md:w-[30rem] sm:w-[20rem]
+     bg-white shadow-lg border border-zinc-300 rounded-xl  '
     >
 
-      <Typography variant="h4">{username} </Typography>
-      <Typography>ID: {id}</Typography>
-      <Typography>Admin: {isSuperadmin ? 'Y' : 'N'}</Typography>
-      <Button disabled variant="contained" sx={{alignSelf: 'end'}}>
+      <div className='text-4xl font-semibold border-b-2 py-2'>{username} </div>
+      <div className='text-lg mt-4'>ID: {id}</div>
+      <div className='text-lg mt-2'>Admin: {isSuperadmin ? 'Y' : 'N'}</div>
+      <button disabled className='bg-zinc-300
+        mt-4 text-white px-5 py-2
+        rounded-xl shadow-lg
+          ' >
         Edit
-      </Button>
-    </Paper>
+      </button>
+    </div>
   );
 };
 
