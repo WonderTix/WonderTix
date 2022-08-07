66a8c36d16fc0904fba55ed333e2e9da45bbf2d2
Michael Budei <77558463+MikeBlackwood@users.noreply.github.com>
2022-08-06 18:41:54 -0700

    Changed render of Manage to get data from Server

--- client/src/components/Ticketing/ticketingmanager/Events/Manageventsmain.tsx
+++ client/src/components/Ticketing/ticketingmanager/Events/Manageventsmain.tsx
@@ -1,15 +1,42 @@
 /* eslint-disable camelcase */
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Udash_nav from '../udash_navbar';
 import ManageEventsPage from './ManageEvents';
 
 const Manageventmain = () => {
-  return (
-    <div className='flex flex-row '>
-      <Udash_nav/>
-      <ManageEventsPage/>
-    </div>
-  );
+  const [eventData, setEventData] = useState();
+  const [loading, setLoading] = useState(true);
+
+  const getDataFromServer = async () => {
+    fetch(process.env.REACT_APP_ROOT_URL +'/api/events/')
+        .then((response) => {
+          return response.json();
+        }).then((data)=>{
+          setEventData(data);
+          console.log('From Manageventmain');
+          console.log(data);
+          setLoading(false);
+        });
+  };
+  useEffect(()=>{
+    getDataFromServer();
+  }, []);
+
+  if (loading === true) {
+    return (
+      <div className='flex flex-row'>
+        <Udash_nav/>
+        <h1>Loading</h1>
+      </div>
+    );
+  } else {
+    return (
+      <div className='flex flex-row'>
+        <Udash_nav/>
+        <ManageEventsPage data={eventData}/>
+      </div>
+    );
+  }
 };
 
 export default Manageventmain;
