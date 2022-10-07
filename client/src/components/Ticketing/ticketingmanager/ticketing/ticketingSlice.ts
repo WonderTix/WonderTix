79b7084daa5502c79f4be9cc348b564c6ef6028a
StormCrozier <63681932+StormCrozier@users.noreply.github.com>
2022-10-06 18:40:39 -0700

    Update ticketingSlice.ts

--- client/src/components/Ticketing/ticketingmanager/ticketing/ticketingSlice.ts
+++ client/src/components/Ticketing/ticketingmanager/ticketing/ticketingSlice.ts
@@ -119,7 +119,7 @@ const updateCartItem = (cart: CartItem[], {id, qty, concessions}: ItemData) =>
         item,
   );
 
-
+/**addTicketReducer adds a ticketReducer to the payload and checks the id similar to qtyReducer*/
 const addTicketReducer: CaseReducer<ticketingState, PayloadAction<{ id: number, qty: number, concessions: boolean}>> = (state, action) => {
   const {id, qty, concessions} = action.payload;
   const tickets = state.tickets;
