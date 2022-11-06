2f5fc428b89182b4d2a37c1786c0b91735300d04
KENDARRR <Kendall.page2@gmail.com>
2022-11-05 17:35:19 -0700

    query change + partial 404 fixes

--- server/src/api/discounts/discounts.router.ts
+++ server/src/api/discounts/discounts.router.ts
@@ -60,13 +60,17 @@ discountsRouter.get('/search', async (
  *
  * @type {?}
  */
-discountsRouter.get('/:code', async(
+discountsRouter.get('/codeCheck', async(
     req: Request,
     res: Response,
 ) => {
     try{
-      const codes = await checkDiscountCode(req.params.code);
-      const code = codes.status.success ? 200 : 404;
+      const codes = await checkDiscountCode(req.query.code);
+      let c1 = codes.status.success ? 200 : 404;
+      if(codes.status.success === true && codes.data.length === 0){
+        c1 = 404;
+      }
+      const code = c1;
       res.status(code).send(codes);
     } catch(error:any) {
       res.status(500).send(error.message);
@@ -130,7 +134,11 @@ discountsRouter.delete('/:id', checkJwt, checkScopes, async (
 ) => {
     try {
       const oldCode = await alterDiscountCode(req.params.id);
-      const code = oldCode.status.success ? 200 : 404;
+      let c1 = oldCode.status.success ? 200 : 404;
+      if(oldCode.status.success === true && oldCode.data.length === 0){
+        c1 = 404;
+      }
+      const code = c1;
       res.status(code).send(oldCode);
     } catch (error: any) {
       res.status(500).send(error.message);
@@ -150,7 +158,11 @@ discountsRouter.delete('/:id', checkJwt, checkScopes, async(
 ) => {
     try {
       const oldCode = await deleteDiscountCode(req.params.id);
-      const code = oldCode.status.success ? 200 : 404;
+      let c1 = oldCode.status.success ? 200 : 404;
+      if(oldCode.status.success === true && oldCode.data.length === 0){
+        c1 = 404;
+      }
+      const code = c1;
       res.status(code).send(oldCode);
     } catch (error: any) {
       res.status(500).send(error.message);
