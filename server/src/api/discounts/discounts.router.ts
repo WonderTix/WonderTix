0546ddfd6b50807c7ecf75c25a75942b74e39da5
KENDARRR <Kendall.page2@gmail.com>
2022-11-10 17:26:39 -0800

    Got left out on accident

--- server/src/api/discounts/discounts.router.ts
+++ server/src/api/discounts/discounts.router.ts
@@ -84,16 +84,16 @@ discountsRouter.put('/:id', checkJwt, checkScopes, async (
     req: Request,
     res: Response,
 ) => {
-  try {
-    const resp = await alterDiscountCode(req.params.id);
-    let tempc = resp.status.success ? 200 : 404;
-    if (tempc === 200 && resp.data.length === 0) {
-      tempc = 404;
+    try {
+      const resp = await alterDiscountCode(req.params.id);
+      let tempc = resp.status.success ? 200 : 404;
+      if(tempc === 200 && resp.data.length === 0){
+        tempc = 404;
+      }
+      const code = tempc;
+    } catch (error: any) {
+      res.status(500).send(error.message);
     }
-    const code = tempc;
-  } catch (error: any) {
-    res.status(500).send(error.message);
-  }
 });
 
 
