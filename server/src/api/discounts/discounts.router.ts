f18a7977243a656314d4d474cf2dac81ba4bf91a
StormCrozier <stormcrozier950@gmail.com>
2022-11-16 18:02:45 -0800

    merge conficts

--- server/src/api/discounts/discounts.router.ts
+++ server/src/api/discounts/discounts.router.ts
@@ -56,25 +56,6 @@ discountsRouter.get('/search', async (
 
 
 /**
- * route: GET /id
- *
- * @type {?}
- */
-discountsRouter.get('/search', async (
-    req: Request,
-    res: Response,
-) => {
-  try {
-    const codes = await checkDiscountCode(req.query.code);
-    const code = codes.status.success ? 200 : 404;
-    res.status(code).send(codes);
-  } catch (error:any) {
-    res.status(500).send(error.message);
-  }
-});
-
-
-/**
  * route: POST /
  *
  * @type {?}
@@ -103,43 +84,19 @@ discountsRouter.put('/:id', checkJwt, checkScopes, async (
     req: Request,
     res: Response,
 ) => {
-    try {
-      const resp = await alterDiscountCode(req.params.id);
-      let code = resp.status.success ? 200 : 404;
-      if(code === 200 && resp.data.length === 0){
-        code = 404;
-        resp.status.success = false;
-      }
-      res.status(code).send(resp);
-    } catch (error: any) {
-      res.status(500).send(error.message);
+  try {
+    const resp = await alterDiscountCode(req.params.id);
+    let code = resp.status.success ? 200 : 404;
+    if (code === 200 && resp.data.length === 0) {
+      code = 404;
+      resp.status.success = false;
     }
+    res.status(code).send(resp);
+  } catch (error: any) {
+    res.status(500).send(error.message);
   }
+},
 );
-
-
-/**
- * route: PUT /
- *
- * @type {?}
- */
-
-discountsRouter.put('/:id', checkJwt, checkScopes, async(
-    req: Request,
-    res: Response,
-) => {
-    try {
-      const oldCode = await alterDiscountCode(req.params.id);
-      let c1 = oldCode.status.success ? 200 : 404;
-      if(oldCode.status.success === true && oldCode.data.length === 0){
-        c1 = 404;
-      }
-      const code = c1;
-      res.status(code).send(oldCode);
-    } catch (error: any) {
-      res.status(500).send(error.message);
-    }
-});
 
 
 /**
@@ -148,21 +105,19 @@ discountsRouter.put('/:id', checkJwt, checkScopes, async(
  * @type {?}
  */
 
-discountsRouter.delete('/:id', checkJwt, checkScopes, async(
+discountsRouter.delete('/:id', checkJwt, checkScopes, async (
     req: Request,
     res: Response,
 ) => {
-    try {
-      const oldCode = await deleteDiscountCode(req.params.id);
-      let c1 = oldCode.status.success ? 200 : 404;
-      if(oldCode.status.success === true && oldCode.data.length === 0){
-        c1 = 404;
-      }
-      const code = c1;
-      res.status(code).send(oldCode);
-    } catch (error: any) {
-      res.status(500).send(error.message);
+  try {
+    const resp = await deleteDiscountCode(req.params.id);
+    let code = resp.status.success ? 200 : 404;
+    if (code === 200 && resp.data.length === 0) {
+      code = 404;
+      resp.status.success = false;
     }
+    res.status(code).send(resp);
+  } catch (error: any) {
+    res.status(500).send(error.message);
+  }
 });
-
-
