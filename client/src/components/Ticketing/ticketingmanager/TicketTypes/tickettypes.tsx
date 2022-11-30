e3e22f6714f53b3afc61f4375a07b7fafe5c9c16
Xavier Reed <xreed18@gmail.com>
2022-11-29 21:26:30 -0800

    confirmation feature and refresh on submit added to page

--- client/src/components/Ticketing/ticketingmanager/TicketTypes/tickettypes.tsx
+++ client/src/components/Ticketing/ticketingmanager/TicketTypes/tickettypes.tsx
@@ -7,6 +7,8 @@ import {
 } from '@mui/x-data-grid';
 import Chip from '@mui/material/Chip';
 import {useAuth0} from '@auth0/auth0-react';
+import {Navigate, NavigationType, useNavigationType} from 'react-router';
+import {useNavigate} from 'react-router-dom';
 
 // Web page that manages ticket types
 const TicketTypes = () => {
@@ -16,6 +18,9 @@ const TicketTypes = () => {
   const [newTicketType, setTicketType] = useState('');
   const [newTicketPrice, setTicketPrice] = useState(0);
   const [newConcessionsPrice, setConcessionsPrice] = useState(0);
+  const [confirmDeletePrompt, setConfirmDeletePrompt] = useState(false);
+  const [cellData, setCellData] = useState<any>([]);
+  const navigate = useNavigate();
 
   // Defines the columns of the grid
   const columns: GridColumns = [
@@ -39,7 +44,11 @@ const TicketTypes = () => {
       renderCell: (cell) => {
         return (
           <Chip label='Delete' color='error' onClick={
-            () => handleDeleteClick(cell)} />
+            () => {
+              setConfirmDeletePrompt(true);
+              setCellData(cell);
+            }
+          } />
         );
       },
     },
@@ -99,10 +108,74 @@ const TicketTypes = () => {
 
       const responseData = await response.json();
       console.log(responseData);
-      getTicketTypes(); // refreshes the page ie re-renders the table
     } catch (error) {
       console.log(error);
     }
+    setConfirmDeletePrompt(false);
+    // refreshes the page ie re-renders the table
+    getTicketTypes();
+  };
+
+
+  const showConfirmDeletePrompt = (cell: GridRenderCellParams) => {
+    console.log('confirm dialog');
+
+    return (
+      <div className='relative w-full h-screen overflow-x-hidden z-10'
+        aria-labelledby="modal-title" role="dialog" aria-modal="true">
+        <div className="fixed z-10 inset-0 overflow-y-auto">
+          <div className="flex items-end sm:items-center justify-center
+            min-h-full p-4 text-center sm:p-0">
+            <div className="relative bg-white rounded-lg text-left
+              overflow-hidden shadow-xl transform transition-all sm:my-8
+              sm:max-w-lg sm:w-full">
+              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div className="sm:flex sm:items-start">
+                  <div className="mx-auto flex-shrink-0 flex items-center
+                    justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0
+                    sm:h-10 sm:w-10">
+                    <svg className="h-6 w-6 text-red-600"
+                      xmlns="http://www.w3.org/2000/svg" fill="none"
+                      viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor"
+                      aria-hidden="true">
+                      <path strokeLinecap="round" strokeLinejoin="round"
+                        d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667
+                        1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34
+                        16c-.77 1.333.192 3 1.732 3z" />
+                    </svg>
+                  </div>
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4
+                    sm:text-left">
+                    <h3 className="text-lg leading-6 font-medium text-gray-900"
+                      id="modal-title">Delete </h3>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-500">Are you sure you
+                        want to delete this?</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex
+              sm:flex-row-reverse">
+                <button onClick={() => handleDeleteClick(cell)} type="button"
+                  className="w-full inline-flex justify-center rounded-md
+                  border border-transparent shadow-sm px-4 py-2 bg-red-600
+                  text-base font-medium text-white hover:bg-red-700
+                  focus:outline-none focus:ring-2 focus:ring-offset-2
+                  focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">Yes</button>
+                <button onClick={() => setConfirmDeletePrompt(false)}
+                  type="button" className="mt-3 w-full inline-flex
+                  justify-center rounded-md border border-gray-300 shadow-sm
+                  px-4 py-2 bg-white text-base font-medium text-gray-700
+                  hover:bg-gray-50 focus:outline-none focus:ring-2
+                  focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3
+                  sm:w-auto sm:text-sm">Cancel</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   };
 
   const handleNameChange = (event) => {
@@ -145,7 +218,10 @@ const TicketTypes = () => {
       );
       if (response.ok) {
         console.log('Ticket type added successfully');
-        setAddTicketClicked(!addTicketClicked); // closes the form
+        // closes the form:
+        setAddTicketClicked(!addTicketClicked);
+        // refreshes the page ie re-renders the table:
+        getTicketTypes();
       }
     } catch (error) {
       console.log(error);
@@ -276,6 +352,7 @@ const TicketTypes = () => {
           onProcessRowUpdateError={(err) => console.log(err)}
           // }}
         />
+        {confirmDeletePrompt && showConfirmDeletePrompt(cellData)}
       </div>
     </div>
   );
