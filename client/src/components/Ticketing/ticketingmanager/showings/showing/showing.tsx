14881afef0b11a34a1b77575425ca139979f3813
Nicole <nicochen@pdx.edu>
2023-03-07 11:08:00 -0800

    Dimmed background when Delete Event modal pops up

--- client/src/components/Ticketing/ticketingmanager/showings/showing/showing.tsx
+++ client/src/components/Ticketing/ticketingmanager/showings/showing/showing.tsx
@@ -157,7 +157,7 @@ const Showing = () => {
         </div>
       </div>
 
-      <div className={!show ? 'hidden': 'fixed w-full h-screen overflow-x-hidden z-10'} aria-labelledby="modal-title" role="dialog" aria-modal="true">
+      <div className={!show ? 'hidden': 'fixed w-full h-screen overflow-x-hidden z-10 bg-gray-500 bg-opacity-75 transition-opacity'} aria-labelledby="modal-title" role="dialog" aria-modal="true">
         <div className="fixed z-10 inset-0 overflow-y-auto">
           <div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
             <div className="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
