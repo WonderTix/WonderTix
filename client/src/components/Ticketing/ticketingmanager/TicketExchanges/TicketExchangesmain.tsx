10894657de23c081a9a4f27be0408eb439d44325
Nicole <nicochen@pdx.edu>
2023-05-23 16:12:20 -0700

    Renamed component

--- client/src/components/Ticketing/ticketingmanager/TicketExchanges/TicketExchangesmain.tsx
+++ client/src/components/Ticketing/ticketingmanager/TicketExchanges/TicketExchangesmain.tsx
@@ -1,14 +1,13 @@
 /* eslint-disable camelcase */
 import React from 'react';
-import Udash_nav from '../../udash_navbar';
-import Showing from './showing';
-const showingmain = () => {
+import Udash_nav from '../udash_navbar';
+
+const TicketExchangesmain = () => {
   return (
     <div className='flex flex-row'>
       <Udash_nav/>
-      <Showing/>
     </div>
   );
 };
 
-export default showingmain;
+export default TicketExchangesmain;
