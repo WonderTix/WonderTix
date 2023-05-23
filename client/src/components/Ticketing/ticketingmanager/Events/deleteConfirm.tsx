8c4467801b87ce2f17c93edf17fba3855b6e13c9
Mathew Chen <mathewchenpnw@gmail.com>
2023-05-22 21:51:43 -0700

    change color for model

--- client/src/components/Ticketing/ticketingmanager/Events/deleteConfirm.tsx
+++ client/src/components/Ticketing/ticketingmanager/Events/deleteConfirm.tsx
@@ -73,9 +73,9 @@ const DeleteConfirm = ({message, setShowConfirm, handleDelete, id}: DeleteConfir
               <button data-modal-toggle="popup-modal"
                 onClick={handleClose}
                 id={id}
-                type="button" className="text-white bg-red-600
-                hover:bg-red-800 focus:ring-4 focus:outline-none
-                focus:ring-red-300 dark:focus:ring-red-800 font-medium
+                type="button" className="text-white bg-gray-600
+                hover:bg-gray-800 focus:ring-4 focus:outline-none
+                focus:ring-gray-300 dark:focus:ring-gray-800 font-medium
                  rounded-lg text-sm inline-flex items-center
                   px-5 py-2.5 text-center mr-2" value="Yes">
                     Yes
