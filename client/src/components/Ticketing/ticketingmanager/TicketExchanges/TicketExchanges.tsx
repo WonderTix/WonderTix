58e8b76aa867d3e2da74f8c6dc8ceea712108dca
Nicole <nicochen@pdx.edu>
2023-05-23 16:30:45 -0700

    Add ticket exchange skeleton layout

---
+++ client/src/components/Ticketing/ticketingmanager/TicketExchanges/TicketExchanges.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const TicketExchanges = () => {
+  return (
+    <div className='w-full h-screen overflow-x-hidden absolute '>
+      <div className='md:ml-[18rem] md:mt-40 sm:mt-[11rem]
+       sm:ml-[5rem] sm:mr-[5rem] sm:mb-[11rem] h-full'>
+        <h1 className='font-bold text-5xl bg-clip-text
+           text-transparent bg-gradient-to-r from-sky-500
+            to-indigo-500 mb-14'>Ticket Exchanges</h1>
+      </div>
+    </div>
+  );
+};
+
+export default TicketExchanges;
--- client/src/components/Ticketing/ticketingmanager/TicketExchanges/TicketExchangesmain.tsx
+++ client/src/components/Ticketing/ticketingmanager/TicketExchanges/TicketExchangesmain.tsx
@@ -1,11 +1,13 @@
 /* eslint-disable camelcase */
 import React from 'react';
 import Udash_nav from '../udash_navbar';
+import TicketExchanges from './TicketExchanges';
 
 const TicketExchangesmain = () => {
   return (
     <div className='flex flex-row'>
       <Udash_nav/>
+      <TicketExchanges/>
     </div>
   );
 };
