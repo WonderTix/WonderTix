c0a838411bb7d3ac31f2af416f44cfee4b0f3909
Nicole <nicochen@pdx.edu>
2023-05-25 11:55:43 -0700

    Add ticket exchange forms

--- client/src/components/Ticketing/ticketingmanager/TicketExchanges/TicketExchanges.tsx
+++ client/src/components/Ticketing/ticketingmanager/TicketExchanges/TicketExchanges.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import TicketExchangeForm from './TicketExchangeForm';
 
 const TicketExchanges = () => {
   return (
@@ -8,6 +9,52 @@ const TicketExchanges = () => {
         <h1 className='font-bold text-5xl bg-clip-text
            text-transparent bg-gradient-to-r from-sky-500
             to-indigo-500 mb-14'>Ticket Exchanges</h1>
+
+        <div className="flex h-fit space-x-4">
+          <div className="w-1/2 border-4
+          border-blue-500 border-opacity-75">
+            <h2 className="text-xl font-bold text-blue-500">
+              Customer&apos;s Ticket
+            </h2>
+            <div className="text-l" id="select-event-id">Select Event</div>
+            <select className="mt-2 border border-gray-300 px-2 py-1 rounded">
+              <option value="">Select Event</option>
+            </select>
+            <div className="text-l" id="select-date-id">Select Date</div>
+            <select className="mt-2 border border-gray-300 px-2 py-1 rounded">
+              <option value="">Select Date</option>
+            </select>
+            <div className="text-l" id="tickets-purchased-list-id">
+              List of Tickets Purchased
+            </div>
+          </div>
+
+          <div className="w-1/2 border-4
+           border-blue-500 border-opacity-75">
+            <h2 className="text-xl font-bold text-blue-500">
+              New Ticket
+            </h2>
+            <div className="text-l" id="select-event-id">Select Event</div>
+            <select className="mt-2 border border-gray-300 px-2 py-1 rounded">
+              <option value="">Select Event</option>
+            </select>
+            <div className="text-l" id="select-date-id">Select Date</div>
+            <select className="mt-2 border border-gray-300 px-2 py-1 rounded">
+              <option value="">Select Date</option>
+            </select>
+            <div className="text-l" id="tickets-purchased-list-id">
+              Tickets Free to Switch
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <button className="float-right bg-green-500
+              hover:bg-green-600 text-white mt-5 py-2 px-4 rounded">
+              Confirm Exchange
+          </button>
+        </div>
+
       </div>
     </div>
   );
