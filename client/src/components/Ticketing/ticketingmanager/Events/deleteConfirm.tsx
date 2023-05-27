5df402b3db50aa4258700c0f993520572ac56e74
Mathew Chen <mathewchenpnw@gmail.com>
2023-05-27 16:00:40 -0700

    change to return and delete

--- client/src/components/Ticketing/ticketingmanager/Events/deleteConfirm.tsx
+++ client/src/components/Ticketing/ticketingmanager/Events/deleteConfirm.tsx
@@ -73,22 +73,22 @@ const DeleteConfirm = ({message, setShowConfirm, handleDelete, id}: DeleteConfir
               <button data-modal-toggle="popup-modal"
                 onClick={handleClose}
                 id={id}
-                type="button" className="text-white bg-red-600
-                hover:bg-red-800 focus:ring-4 focus:outline-none
-                focus:ring-red-300 dark:focus:ring-red-800 font-medium
-                 rounded-lg text-sm inline-flex items-center
-                  px-5 py-2.5 text-center mr-2" value="Yes">
-                    Yes
-              </button>
-              <button data-modal-toggle="popup-modal"
-                id={id}
-                onClick={handleClose}
                 type="button" className="text-white bg-gray-600
                 hover:bg-gray-800 focus:ring-4 focus:outline-none
                 focus:ring-gray-300 dark:focus:ring-gray-800 font-medium
                  rounded-lg text-sm inline-flex items-center
                   px-5 py-2.5 text-center mr-2" value="No">
-                    No
+                    Return
+              </button>
+              <button data-modal-toggle="popup-modal"
+                id={id}
+                onClick={handleClose}
+                type="button" className="text-white bg-red-600
+                hover:bg-red-800 focus:ring-4 focus:outline-none
+                focus:ring-red-300 dark:focus:ring-red-800 font-medium
+                 rounded-lg text-sm inline-flex items-center
+                  px-5 py-2.5 text-center mr-2" value="Yes">
+                    Delete
               </button>
             </div>
           </div>
