ecb06f718211465f410bb2ad2447e2f011326e06
Mathew Chen <mathewchenpnw@gmail.com>
2023-05-29 16:58:56 -0700

    change popup color

--- client/src/components/Ticketing/Pop-up.tsx
+++ client/src/components/Ticketing/Pop-up.tsx
@@ -70,9 +70,9 @@ const PopUp = ({title, message, handleClose, handleProceed}: popupProps) => {
               </p>
               <button data-modal-toggle="popup-modal"
                 onClick={handleProceed}
-                type="button" className="text-white bg-green-600
-                hover:bg-green-800 focus:ring-4 focus:outline-none
-                focus:ring-green-300 dark:focus:ring-green-800 font-medium
+                type="button" className="text-white bg-red-600
+                hover:bg-red-800 focus:ring-4 focus:outline-none
+                focus:ring-red-300 dark:focus:ring-red-800 font-medium
                  rounded-lg text-sm inline-flex items-center
                   px-5 py-2.5 text-center mr-2">
                     Proceed
