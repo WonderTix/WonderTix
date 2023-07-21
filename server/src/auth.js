acb29b8ca23597b8a787fac93c77882e8f898108
lee-112 <leeian@pdx.edu>
2023-07-20 21:44:33 -0700

    Update auth.js
    Updated issuerBaseURL

--- server/src/auth.js
+++ server/src/auth.js
@@ -10,6 +10,11 @@ exports.checkScopes = exports.checkJwt = void 0;
 var express_oauth2_jwt_bearer_1 = require("express-oauth2-jwt-bearer");
 exports.checkJwt = (0, express_oauth2_jwt_bearer_1.auth)({
     audience: 'https://localhost:8000',
+<<<<<<< Updated upstream
     issuerBaseURL: "https://dev-ioqpbmj3lwv6zkir.us.auth0.com"
+=======
+    issuerBaseURL: 'https://dev-6tbihuu3m3ttok1i.us.auth0.com'
+    // issuerBaseURL: "https://wtix-dev.us.auth0.com/"
+>>>>>>> Stashed changes
 });
 exports.checkScopes = (0, express_oauth2_jwt_bearer_1.requiredScopes)('admin');
