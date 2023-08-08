6b76fd8467faf4b5870246315a875b52510a4cac
JohnN <66976803+xiivi@users.noreply.github.com>
2023-08-07 20:02:43 -0700

    Don donor dashboard branch (#325)

--- client/src/components/DonorManagement/Dashboard.tsx
+++ client/src/components/DonorManagement/Dashboard.tsx
@@ -1,1 +1,16 @@
-export { };
+/**
+ * Donor Management Dashboard
+ *
+ * @returns {React.ReactElement}
+ */
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>
+        hello world
+      </h1>
+    </div>
+  );
+};
+
+export default Dashboard;
