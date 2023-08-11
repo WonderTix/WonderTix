b37c137918cdb05b4ccfa9a83cf122fc40f4d0c5
ChentaoMa <93690199+ChentaoMa@users.noreply.github.com>
2023-08-11 11:42:47 -0700

    Update example.spec.ts

--- client/tests/example.spec.ts
+++ client/tests/example.spec.ts
@@ -16,3 +16,35 @@ test('get started link', async ({ page }) => {
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*intro/);
 });
+
+test('portland play house link',async({page})=>{
+  await page.goto('https://localhost:3000/');
+
+  await page.getByRole('link', { name: '/' }).click();
+
+  await expect(page).toHaveURL('https://portlandplayhouse.org/');
+});
+
+test('donate',async({page})=>{
+  await page.goto('https://localhost:3000/');
+
+  await page.getByRole('button', { name: 'Donate' }).click();
+
+  await expect(page).toHaveURL('https://localhost:3000/donate');
+
+  await page.getByRole('button', { name: 'Back to Events' }).click();
+
+  await expect(page).toHaveURL('https://localhost:3000/');
+});
+
+test('events',async({page})=>{
+  await page.goto('https://localhost:3000/');
+
+  await page.getByRole('button', { name: 'See Showings' }).first().click();
+
+  await expect(page).toHaveURL('https://localhost:3000/events/32');
+
+  await page.getByRole('button', { name: 'Back to Events' }).click();
+
+  await expect(page).toHaveURL('https://localhost:3000/');
+});
