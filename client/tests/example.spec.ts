212432146761ec1f8ed9e581bde463dde0f23bcf
ChentaoMa <93690199+ChentaoMa@users.noreply.github.com>
2023-08-14 21:57:48 -0700

    Update example.spec.ts

--- client/tests/example.spec.ts
+++ client/tests/example.spec.ts
@@ -1,20 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('portland play house link',async({page})=>{
+  await page.goto('https://localhost:3000/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  await page.getByRole('link', { name: '/' }).click();
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+  await expect(page).toHaveURL('https://portlandplayhouse.org/');
 });
 
 test('donate',async({page})=>{
