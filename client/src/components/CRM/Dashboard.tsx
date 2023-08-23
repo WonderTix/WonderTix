74ed4217363e8ea23941bf4a0b9c6bf9e1314e0e
Henry Kaus <92124260+henrykaus@users.noreply.github.com>
2023-08-23 16:26:27 -0700

    [TIC-56] Refactor Upper Admin Nav and other UI fixes (#351)

--- client/src/components/CRM/Dashboard.tsx
+++ client/src/components/CRM/Dashboard.tsx
@@ -10,13 +10,11 @@ const Dashboard = (): ReactElement => {
   return (
     <div className="w-full h-screen overflow-x-hidden absolute">
       <div className="md:ml-[22rem] md:mt-40 sm:my-[11rem] sm:mx-[5rem]">
-        <h1 className="font-bold text-5xl mb-14 pb-8 w-[60rem]">
-          Admin Dashboard
-        </h1>
+        <h1 className="font-bold text-5xl mb-14 pb-8">Admin Dashboard</h1>
         <div className="grid md:grid-cols-2 sm:grid-cols-1 gap-5">
           <DashboardLink
             gradientClasses="bg-gradient-to-r from-sky-500 to-indigo-500"
-            icon={(
+            icon={
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-12 w-12"
@@ -31,14 +29,14 @@ const Dashboard = (): ReactElement => {
                   d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
                 />
               </svg>
-            )}
+            }
             size="lg"
             title="Accounts"
             route="/admin/accounts"
           />
           <DashboardLink
             gradientClasses="bg-gradient-to-r from-cyan-500 to-blue-500"
-            icon={(
+            icon={
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-12 w-12"
@@ -53,14 +51,14 @@ const Dashboard = (): ReactElement => {
                   d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2"
                 />
               </svg>
-            )}
+            }
             size="lg"
             title="Contacts"
             route="/admin/contacts"
           />
           <DashboardLink
             gradientClasses="bg-gradient-to-r from-violet-500 to-fuchsia-500"
-            icon={(
+            icon={
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-12 w-12"
@@ -75,14 +73,14 @@ const Dashboard = (): ReactElement => {
                   d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
                 />
               </svg>
-            )}
+            }
             size="md"
             title="Reporting"
             route="/admin/reporting"
           />
           <DashboardLink
             gradientClasses="bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500"
-            icon={(
+            icon={
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-12 w-12"
@@ -97,14 +95,14 @@ const Dashboard = (): ReactElement => {
                   d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
-            )}
+            }
             size="md"
             title="Create Task"
             route="/admin/tasks/create"
           />
           <DashboardLink
             gradientClasses="bg-gradient-to-r from-yellow-600 to-red-600"
-            icon={(
+            icon={
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-12 w-12"
@@ -118,13 +116,13 @@ const Dashboard = (): ReactElement => {
                   clipRule="evenodd"
                 />
               </svg>
-            )}
+            }
             title="Edit Task"
             route="/admin/tasks/edit"
           />
           <DashboardLink
             gradientClasses="bg-gradient-to-r from-purple-400 to-yellow-400"
-            icon={(
+            icon={
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-12 w-12"
@@ -139,7 +137,7 @@ const Dashboard = (): ReactElement => {
                   d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
-            )}
+            }
             title="Account Information"
             route="/admin/tasks/accountInformation"
           />
--- client/src/components/Ticketing/ticketingmanager/dashboard.tsx
+++ client/src/components/Ticketing/ticketingmanager/dashboard.tsx
@@ -12,13 +12,11 @@ const Dashboard = (): ReactElement => {
   return (
     <div className="w-full h-screen overflow-x-hidden absolute">
       <div className="md:ml-[22rem] md:mt-40 sm:mx-[5rem] sm:my-[11rem]">
-        <h1 className="font-bold text-5xl mb-14 pb-8 w-[60rem]">
-          Ticketing Dashboard
-        </h1>
+        <h1 className="font-bold text-5xl mb-14 pb-8">Ticketing Dashboard</h1>
         <div className="grid md:grid-cols-2 sm:grid-cols-1 gap-5">
           <DashboardLink
             gradientClasses="bg-gradient-to-r from-sky-500 to-indigo-500"
-            icon={(
+            icon={
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-12 w-12"
@@ -32,14 +30,14 @@ const Dashboard = (): ReactElement => {
                   clipRule="evenodd"
                 />
               </svg>
-            )}
+            }
             size="lg"
             title="Door List"
             route="/ticketing/doorlist"
           />
           <DashboardLink
             gradientClasses="bg-gradient-to-r from-cyan-500 to-blue-500"
-            icon={(
+            icon={
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-12 w-12"
@@ -54,14 +52,14 @@ const Dashboard = (): ReactElement => {
                   d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z"
                 />
               </svg>
-            )}
+            }
             size="lg"
             title="Showings"
             route="/ticketing/showings"
           />
           <DashboardLink
             gradientClasses="bg-gradient-to-r from-violet-500 to-fuchsia-500"
-            icon={(
+            icon={
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-12 w-12"
@@ -76,14 +74,14 @@ const Dashboard = (): ReactElement => {
                   d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
-            )}
+            }
             size="md"
             title="Add New Events"
             route="/ticketing/addevent"
           />
           <DashboardLink
             gradientClasses="bg-gradient-to-r from-green-500 to-zinc-500"
-            icon={(
+            icon={
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-12 w-12"
@@ -98,14 +96,14 @@ const Dashboard = (): ReactElement => {
                   d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
                 />
               </svg>
-            )}
+            }
             size="md"
             title="Purchase Tickets"
             route="/ticketing/purchaseticket"
           />
           <DashboardLink
             gradientClasses="bg-gradient-to-r from-yellow-600 to-red-600"
-            icon={(
+            icon={
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-12 w-12"
@@ -114,13 +112,13 @@ const Dashboard = (): ReactElement => {
               >
                 <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM14 11a1 1 0 011 1v1h1a1 1 0 110 2h-1v1a1 1 0 11-2 0v-1h-1a1 1 0 110-2h1v-1a1 1 0 011-1z" />
               </svg>
-            )}
+            }
             title="Create Newsletter"
             route="/ticketing/addnewsletter"
           />
           <DashboardLink
             gradientClasses="bg-gradient-to-r from-purple-400 to-yellow-400"
-            icon={(
+            icon={
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-12 w-12"
@@ -134,13 +132,13 @@ const Dashboard = (): ReactElement => {
                   clipRule="evenodd"
                 />
               </svg>
-            )}
+            }
             title="Manage Seasonal Tickets"
             route="/ticketing/seasonaltickets"
           />
           <DashboardLink
             gradientClasses="bg-gradient-to-r from-green-400 to-teal-700"
-            icon={(
+            icon={
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-12 w-12"
@@ -154,13 +152,13 @@ const Dashboard = (): ReactElement => {
                   clipRule="evenodd"
                 />
               </svg>
-            )}
+            }
             title="Manage Ticket Types"
             route="/ticketing/tickettypes"
           />
           <DashboardLink
             gradientClasses="bg-gradient-to-r from-red-500 to-blue-600"
-            icon={(
+            icon={
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-12 w-12"
@@ -175,7 +173,7 @@ const Dashboard = (): ReactElement => {
                   d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z"
                 />
               </svg>
-            )}
+            }
             title="Ticket Exchanges"
             route="/ticketing/ticketexchanges"
           />
