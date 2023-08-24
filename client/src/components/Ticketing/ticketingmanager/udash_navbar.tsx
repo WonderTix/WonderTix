9b13734671202366cff650fd134d1fc87bcfb974
benaustin <bena2@pdx.edu>
2023-08-24 15:39:34 -0700

    TIC-48: Updated Navbar to 'Events' instead of 'Showings'

--- client/src/components/Ticketing/ticketingmanager/udash_navbar.tsx
+++ client/src/components/Ticketing/ticketingmanager/udash_navbar.tsx
@@ -113,7 +113,7 @@ const Udash_nav = () => {
                       d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z"
                     />
                   </svg>
-                  <span>Showings</span>
+                  <span>Events</span>
                 </div>
               </a>
             </li>
