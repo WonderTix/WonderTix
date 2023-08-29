db39ee4d733c8f502bc21989d6ae2587859247df
jonathantnguyen <jonnytnguyen@gmail.com>
2023-08-28 19:11:53 -0700

    refactoring

--- client/src/components/Ticketing/mainpage/hero.tsx
+++ client/src/components/Ticketing/mainpage/hero.tsx
@@ -39,15 +39,15 @@ const Hero = () => {
     }
   };
   return (
-    <div className = 'home w-full h-screen ' >
-      <div className=' w-full h-screen bg-zinc-100
+    <div className='home w-full h-screen ' >
+      <div className='w-full h-screen bg-zinc-100
        overflow-y-hidden overflow-x-hidden sm:bg-scroll
-        justify-between md:bg-fixed bg-cover bg-hero bg-brightness-50' >
+        justify-between md:bg-fixed bg-cover bg-hero bg-brightness-50'>
         <div className='flex flex-row md:flex-row sm:flex-col
          sm:items-center w-full h-full bg-gradient-to-r from-black'>
           <div className='max-w-[1240px] md:pl-40 flex flex-row
-            text-center  text-white m-auto '>
-            <div className = 'flex flex-col justify-center md:items-center w-full px-2 py8 md:mr-40 md:mt-auto sm:mt-40 sm:mb-10'>
+            text-center text-white m-auto'>
+            <div className= 'flex flex-col justify-center md:items-center w-full px-2 py8 md:mr-40 md:mt-auto sm:mt-40 sm:mb-10'>
               <h1 className='text-5xl md:text-7xl font-bold'>Events</h1>
               <div className='flex flex-col my-2'>
                 <label className='text-zinc-200/60  px-10 my-1'>Pick an Event</label>
