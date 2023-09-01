ab29ace98c7a30d71c53fdd1622cbb3cd551418b
txiong002 <tfx22@yahoo.com>
2023-09-01 16:49:57 -0700

    TIC-84: fixed spacing on continue button on donations page, when in mobile view

--- client/src/components/Ticketing/donation/DonationPage.tsx
+++ client/src/components/Ticketing/donation/DonationPage.tsx
@@ -49,7 +49,7 @@ export default function DonationPage({onNext}: {onNext: () => any}): ReactElemen
           Please consider making a donation
         </div>
 
-        <div className='flex flex-row items-center rounded-xl w-[15rem] p-4 bg-zinc-100 text-zinc-500'>
+        <div className='flex flex-row items-center rounded-xl w-[15rem] p-4 mb-4 bg-zinc-100 text-zinc-500'>
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
