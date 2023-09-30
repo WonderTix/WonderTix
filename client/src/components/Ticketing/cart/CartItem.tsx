d1b9b824ed6945590933204e930922897f7940a5
Henry Kaus <henrykaus19@gmail.com>
2023-09-30 16:26:53 -0700

    TIC-82: updated spacing and alignment of cart and cartitem elements

--- client/src/components/Ticketing/cart/CartItem.tsx
+++ client/src/components/Ticketing/cart/CartItem.tsx
@@ -43,18 +43,16 @@ const CartRow = ({item, removeHandler}: CartRowProps) => {
   };
 
   return (
-    <div className='bg-zinc-200 w-full flex flex-row
-     h-40 gap-5 rounded-xl bg-cover' style={{backgroundImage: `url(${getImageDefault(item.product_img_url)}),url(${getImageDefault()})`}}>
-      <div className='flex flex-col md:flex-row sm:flex-col
-         sm:items-center w-full rounded-xl  bg-zinc-900/90 overflow-y-scroll justify-between'>
-        <span className='ml-5'>
-          <div className='text-white font-semibold'>
+    <div className='w-full h-40 rounded-xl bg-cover' style={{backgroundImage: `url(${getImageDefault(item.product_img_url)}),url(${getImageDefault()})`}}>
+      <div className='flex flex-col gap-5 p-5 justify-center items-center h-full md:flex-row md:justify-between rounded-xl bg-zinc-900/90'>
+        <span>
+          <p className='text-white font-semibold'>
             {item.name}
-          </div>
+          </p>
           <p className='text-zinc-200'>{item.desc}</p>
         </span>
-        <div className='flex flex-row items-center gap-7 mr-5'>
-          <div className='flex flex-row items-center gap-2'>
+        <span className='flex items-center gap-7'>
+          <div className='flex items-center gap-2'>
             <button className='text-white items-center' aria-label={`remove one ${item.name} to cart`} onClick={decrement}>
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M5 10a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1z" clipRule="evenodd" />
@@ -81,8 +79,7 @@ const CartRow = ({item, removeHandler}: CartRowProps) => {
               <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
             </svg>
           </button>
-        </div>
-
+        </span>
       </div>
     </div>
   );
