67c76d9a5d4803233e159233389142768b8f04a0
Jaeger Tang <jaeger@tozny.com>
2023-10-01 18:16:16 -0700

    [DEP-49] more edits

--- client/e2e/auth.setup.ts
+++ client/e2e/auth.setup.ts
@@ -5,14 +5,11 @@ import path from 'path';
 
 // Construct the path to the .env file based on __dirname
 console.log(`process env CI is ${process.env.CI}`);
-console.log(`process env CI is ${process.env.CI}`);
-console.log(`process env CI is ${process.env.CI}`);
 if (!process.env.CI) {
-  console.log("using dotenv");
-  console.log("using dotenv");
-  dotenv.config({path: path.join(__dirname, '../../.env')});
+  console.log('using dotenv');
+  const envPath = path.join(__dirname, '../../.env');
+  dotenv.config({path: envPath});
 }
-console.log(`process env is ${process.env}`);
 
 /**
  * https://playwright.dev/docs/auth
@@ -22,10 +19,10 @@ const authFile = 'playwright/.auth/user.json';
 
 setup('authenticate', async ({page}) => {
   setup.setTimeout(120000);
-   
+
   const loginPage = new LoginPage(page);
 
-  const email = process.env.TEST_EMAIL as string;
+  const email = process.env.TEST_USERNAME as string;
   const password = process.env.TEST_PASSWORD as string;
 
   await loginPage.login(email, password);
@@ -39,4 +36,3 @@ setup('authenticate', async ({page}) => {
   // Store the authentication state for future use.
   await page.context().storageState({path: authFile});
 });
-
