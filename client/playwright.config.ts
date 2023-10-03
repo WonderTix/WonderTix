7baf783b3e80dbcf9ac4ae6f29c034eb6879fb3b
Jaeger Tang <jaeger@tozny.com>
2023-10-02 20:07:47 -0700

    [DEP-49] changed artifact settings
    Co-authored-by: Justin Greever <jgreever@gmail.com>

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', {outputFolder: 'test-results/'}]],
+  reporter: [['html', {outputFolder: 'playwright'}]],
   outputDir: 'test-results',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
