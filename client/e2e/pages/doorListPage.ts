e1e75e9f8ff0889e767429fb27ba2282b1cfd7db
snorq2 <81600811+snorq2@users.noreply.github.com>
2023-10-03 16:54:41 -0700

    DEP-52 Filling out further elements of the object

--- client/e2e/pages/doorListPage.ts
+++ client/e2e/pages/doorListPage.ts
@@ -5,10 +5,28 @@ export class DoorListPage {
   readonly page: Page;
 
   readonly pageHeader: Locator;
+  readonly chooseEvent: Locator;
+  readonly chooseTime: Locator;
 
   constructor(page: Page) {
     this.page = page;
 
     this.pageHeader = page.getByRole('heading', {name: 'Door List'});
+    this.chooseEvent = page.getByLabel('Choose Event');
+    this.chooseTime = page.getByLabel('Choose Time');
+  }
+
+  async goto() {
+    await this.page.goto('https://localhost:3000/ticketing/doorlist', {timeout: 90000});
+  }
+
+  //  Will need to determine away to identify event specitically.
+  async setEvent(event: string) {
+    await this.chooseEvent.selectOption(event);
+  }
+
+  //  Also need a way to identify the time
+  async setTime(time: string) {
+    await this.chooseTime.selectOption(time);
   }
 }
