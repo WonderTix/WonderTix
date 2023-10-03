9fd152e91d1c421fae7dea21d5240d05beea15b6
snorq2 <81600811+snorq2@users.noreply.github.com>
2023-10-02 21:39:17 -0700

    DEP-52 Set up POM skeleton

--- client/e2e/pages/doorListPage.ts
+++ client/e2e/pages/doorListPage.ts
@@ -5,46 +5,10 @@ export class DoorListPage {
   readonly page: Page;
 
   readonly pageHeader: Locator;
-  readonly chooseEvent: Locator;
-  readonly chooseTime: Locator;
 
   constructor(page: Page) {
     this.page = page;
 
     this.pageHeader = page.getByRole('heading', {name: 'Door List'});
-    this.chooseEvent = page.getByLabel('Choose Event');
-    this.chooseTime = page.getByLabel('Choose Time');
-  }
-
-  async goto() {
-    await this.page.goto('https://localhost:3000/ticketing/doorlist', {timeout: 90000});
-  }
-
-  //  Will need to determine away to identify event specitically.
-  async setEvent(event: string) {
-    await this.chooseEvent.selectOption(event);
-  }
-
-  //  Also need a way to identify the time
-  async setTime(time: string) {
-    await this.chooseTime.selectOption(time);
-  }
-
-  async getHeader() {
-    return this.pageHeader.textContent();
-  }
-
-  async selectRandomShow() {
-    const shows = this.chooseEvent.allInnerTexts();
-    const randShow = shows[Math.floor(Math.random() * (await shows).length)];
-    this.setEvent(randShow);
-    return this.chooseEvent.textContent();
-  }
-
-  async selectRandomTime() {
-    const times = this.chooseTime.allInnerTexts();
-    const randTime = times[Math.floor(Math.random() * (await times).length)];
-    this.setTime(randTime);
-    return this.chooseTime.textContent();
   }
 }
