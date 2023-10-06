a025fcf24a7e69f48af18d45385a333cb3985728
snorq2 <81600811+snorq2@users.noreply.github.com>
2023-10-05 22:59:44 -0700

    DEP-52 Wrote initial tests Experimenting with locators

--- client/e2e/doorListTest.spec.ts
+++ client/e2e/doorListTest.spec.ts
@@ -4,15 +4,13 @@ import {DoorListPage} from './pages/doorListPage';
 
 test('Check Home', async ({page}) => {
   const doorList = new DoorListPage(page);
-  await doorList.goto();
-  expect(await doorList.getHeader, 'Door List');
+  expect(doorList.getHeader, 'Door List');
 });
 
 test('random show and time', async ({page}) => {
   const doorList = new DoorListPage(page);
-  await doorList.goto();
-  const showing = await doorList.selectRandomShow();
+  const showing = doorList.selectRandomShow();
   console.log(await showing);
-  const time = await doorList.selectRandomTime();
+  const time = doorList.selectRandomTime();
   console.log(await time);
 });
