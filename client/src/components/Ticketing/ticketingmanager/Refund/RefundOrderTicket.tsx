a3a86468de16fd28ed2ffc8730ab72023310a215
jonathantnguyen <jonnytnguyen@gmail.com>
2023-10-08 12:10:11 -0700

    Refactoring

--- client/src/components/Ticketing/ticketingmanager/Refund/RefundOrderTicket.tsx
+++ client/src/components/Ticketing/ticketingmanager/Refund/RefundOrderTicket.tsx
@@ -14,17 +14,17 @@ const Refund = () => {
               Stripe Order
             </h2>
             <div className='flex flex-wrap w-64 mx-auto'>
-              <label htmlFor='payment-intent' className='text-black text-l' id='payment-input'>
+              <label htmlFor='payment-intent' className='text-white text-l' id='payment-input'>
                 Payment Intent
-              <input className='w-full mt-1 mb-3 px-2 py-2 rounded' type='text'
+              <input className='text-black w-full mt-1 mb-3 px-2 py-2 rounded' type='text'
                 id='payment-intent' name='payment-intent'
                 placeholder='XXXXX...'></input>
               </label>
             </div>
             <div className='flex flex-wrap w-64 mx-auto'>
-              <label htmlFor='refund-intent' className='text-black text-l' id='refund-input'>
+              <label htmlFor='refund-intent' className='text-white text-l' id='refund-input'>
                 Refund Intent
-              <input className='w-full mt-1 mb-3 px-2 py-2 rounded' type='text'
+              <input className='text-black w-full mt-1 mb-3 px-2 py-2 rounded' type='text'
                 id='refund-intent' name='refund-intent'
                 placeholder='XXXXX...'></input>
               </label>
