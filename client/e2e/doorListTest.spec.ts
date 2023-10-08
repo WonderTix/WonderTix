7d2c249ad6679981125ecd122fd47aebca2539e8
snorq2 <81600811+snorq2@users.noreply.github.com>
2023-10-07 21:12:30 -0700

    DEP-52 Selection randomizer working

--- client/e2e/doorListTest.spec.ts
+++ client/e2e/doorListTest.spec.ts
@@ -4,13 +4,15 @@ import {DoorListPage} from './pages/doorListPage';
 
 test('Check Home', async ({page}) => {
   const doorList = new DoorListPage(page);
-  expect(doorList.getHeader, 'Door List');
+  await doorList.goto();
+  expect(await doorList.getHeader, 'Door List');
 });
 
 test('random show and time', async ({page}) => {
   const doorList = new DoorListPage(page);
-  const showing = doorList.selectRandomShow();
+  await doorList.goto();
+  const showing = await doorList.selectRandomShow();
   console.log(await showing);
-  const time = doorList.selectRandomTime();
+  const time = await doorList.selectRandomTime();
   console.log(await time);
 });
