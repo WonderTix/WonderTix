c095750114b063db77835b1de9ae8ebbec9d3c23
Jonathan Nguyen <jonguyen@pdx.edu>
2023-10-08 11:43:17 -0700

    TIC-86 Added Refund Button/Page to Ticket Manager (#388)
    Things may adjust once TIC-62 is merged.

---
+++ client/src/components/Ticketing/ticketingmanager/Refund/RefundOrderTicketMain.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Udash_nav from '../udash_navbar';
+import Refund from './RefundOrderTicket';
+
+/**
+ * Hosts refund page
+ *
+ * @returns {Navbar, RefundPage, Footer}
+ */
+const RefundMain = () => {
+  return (
+    <div className='flex flex-row'>
+      <Udash_nav/>
+      <Refund/>
+    </div>
+  );
+};
+
+export default RefundMain;
