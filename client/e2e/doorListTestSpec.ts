826e0ed50117f718d178a49563f3f76527d9042d
snorq2 <81600811+snorq2@users.noreply.github.com>
2023-10-08 14:04:43 -0700

    DEP-52 ignoring the Wondertix index.html

--- client/e2e/doorListTestSpec.ts
+++
@@ -1,8 +0,0 @@
-import {test, expect} from '@playwright/test';
-import {DoorListPage} from './pages/doorListPage';
-
-
-test('Check Home', async({page}) => {
-  const doorList = new DoorListPage(page);
-  
-});
