456e0a083b80fa942c12f8e5123386946a14ae27
snorq2 <81600811+snorq2@users.noreply.github.com>
2023-10-08 17:25:15 -0700

    DEP-52 added label to event time selector refactored random selector into helper function implemented first main page tests

--- client/e2e/mainTest.spec.ts
+++ client/e2e/mainTest.spec.ts
@@ -4,6 +4,14 @@ import {MainPage} from './pages/mainPage';
 test('Check Home', async ({page}) => {
   const main = new MainPage(page);
   await main.goto();
+  expect(await main.headingEvent).toBeVisible();
+});
+
+test('view show', async ({page}) => {
+  const main = new MainPage(page);
+  await main.goto();
+  const showing = await main.goFirstShowing();
+  expect(showing).not.toBeNull();
 });
 
 test('add ticket', async ({page}) => {
@@ -11,4 +19,10 @@ test('add ticket', async ({page}) => {
   await main.goto();
   const showing = await main.goFirstShowing();
   console.log(showing);
+  const date = await main.selectRandomDate();
+  console.log(date);
+  const ticketType = await main.selectRandomTicketType();
+  console.log(ticketType);
+  const quantity = await main.selectRandomQuantity();
+  console.log(quantity);
 });
--- client/e2e/pages/mainPage.ts
+++ client/e2e/pages/mainPage.ts
@@ -6,31 +6,79 @@ export class MainPage {
   readonly page: Page;
 
   readonly firstShowing: Locator;
+  readonly headingEvent: Locator;
+
+  // Below elements are actually on the event template
+  // Should event template be its own page object?
   readonly selectDate: Locator;
+  readonly selectTime: Locator;
   readonly selectTicketType: Locator;
   readonly selectQuantity: Locator;
   readonly addConcessionsTicket: Locator;
   readonly getTickets: Locator;
-  readonly headingEvent: Locator;
+  readonly titleEvent: Locator;
 
   constructor(page: Page) {
     this.page = page;
 
     this.firstShowing = page.getByRole('button', {name: 'See Showings'}).first();
-    this.selectDate = page.getByRole('combobox', {name: 'select-date'});
-    this.selectTicketType = page.getByRole('combobox', {name: 'select-ticket-type'});
-    this.selectQuantity = page.getByRole('combobox', {name: 'select-qty'});
-    this.addConcessionsTicket = page.getByRole('checkbox', {name: 'checkbox-concessions'});
-    this.getTickets = page.getByRole('button', {name: 'Get Tickets'});
     this.headingEvent = page.getByRole('heading', {name: 'Events'});
+    this.selectDate = page.locator('#select-date');
+    this.selectTime = page.locator('#select-time');
+    this.selectTicketType = page.locator('#select-ticket-type');
+    this.selectQuantity = page.locator('#select-qty');
+    this.addConcessionsTicket = page.locator('#checkbox-concessions');
+    this.getTickets = page.getByRole('button', {name: 'Get Tickets'});
+    this.titleEvent = page.locator('#event-title');
   }
 
   async goto() {
     await this.page.goto('/', {timeout: 90000});
-    await expect(this.headingEvent).toBeVisible;
   }
 
   async goFirstShowing() {
     await this.firstShowing.click();
+    const title = await this.titleEvent.textContent();
+    return await title;
   }
+
+  private async selectRandomOption(optionBox: Locator) {
+    const allOptions = await (await optionBox.allInnerTexts())[0].split('\n');
+    allOptions.shift();
+    const randomOption = allOptions[Math.floor(Math.random() * (await allOptions).length)];
+    await optionBox.selectOption({label: randomOption});
+    return await randomOption;
+  }
+
+  async selectRandomDate() {
+    const randomDate = await this.selectRandomOption(this.selectDate);
+    return await randomDate;
+  }
+
+  async selectRandomTime() {
+    const randomTime = await this.selectRandomOption(this.selectTime);
+    return await randomTime;
+  }
+
+  async selectRandomTicketType() {
+    const randomTicketType = await this.selectRandomOption(this.selectTicketType);
+    return await randomTicketType;
+  }
+
+  async selectRandomQuantity() {
+    const randonQuantity = await this.selectRandomOption(this.selectQuantity);
+    return await randonQuantity;
+  }
+
+  async clickGetTickets() {
+    await this.getTickets.click();
+  }
+
+  //   async selectRandomShow() {
+  //   const showsUnit = await this.chooseEvent.allInnerTexts();
+  //   const shows = showsUnit[0].split('\n');
+  //   const randShow = shows[Math.floor(Math.random() * (await shows).length)];
+  //   await this.setEventByLabel(randShow);
+  //   return await randShow;
+  // }
 }
