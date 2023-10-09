4d452746a93fe4d0070122fe74a45c646b0c879f
Aidan Grieve <aidan@Aidans-MacBook-Pro.local>
2023-10-08 17:09:25 -0700

    continued work on report layout

--- client/src/components/CRM/Reporting/CreditCardRecon.tsx
+++ client/src/components/CRM/Reporting/CreditCardRecon.tsx
@@ -21,20 +21,44 @@ const CreditCardRecon=() =>{
                     'width': '100%',
                     'height': '100%',
                     'margin-left': '16.7rem',
-                    'margin-top': '4rem',
+                    'margin-right': '4rem',
                 }}
             >
                 <Box
                     sx = {{
-                        'margin-top': '6em',
-                        'margin-bottom': '6em',
+                        'margin-top': '6rem',
+                        'margin-bottom': '1.5rem',
                     }}
                 >
-                    <h1 className='font-bold text-5xl bg-clip-text text-transparent bg-gradient-to-r from-violet-500 to-fuchsia-500 mb-10 pb-4'>
+                    <h1 className='font-bold text-5xl bg-clip-text text-transparent bg-gradient-to-r from-violet-500 to-fuchsia-500'>
                     Credit Card Reconciliation Report
                     </h1>
                 </Box>
-                <Grid>
+                <Grid
+                    container
+                    direction="row"
+                    justifyContent="flex-start"
+                >
+                    <Grid item xs={1.90}>
+                        <Box
+                            sx = {{
+                                'width': '90%',
+                                'height': '5rem',
+                                'background-color': 'red',
+                            }}
+                        />
+                        <h1>Filter</h1>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <Box
+                            sx = {{
+                                'width': '90%',
+                                'height': '10rem',
+                                'background-color': 'blue',
+                            }}
+                        />
+                        <h1>Report</h1>
+                    </Grid>
                 </Grid>
             </Box>
         </Box>
