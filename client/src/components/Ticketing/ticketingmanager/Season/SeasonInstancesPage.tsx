a3ff9f9e10aa350d455e7a3b13f5d5913407debd
Hali Tran <78198010+HaliTran@users.noreply.github.com>
2023-10-10 12:01:28 -0700

    TIC-77: Fix mobile responsiveness issues and add select

--- client/src/components/Ticketing/ticketingmanager/Season/SeasonInstancesPage.tsx
+++ client/src/components/Ticketing/ticketingmanager/Season/SeasonInstancesPage.tsx
@@ -52,24 +52,29 @@ const SeasonInstancesPage = () => {
     getAllSeasons();
   }, [token]);
 
+  /**
+   * Based on active/inactive/all
+   *
+   * @param event
+   */
+  const handleEventChange = (event) => {
+    // TODO: handle the season type change when active/inactive is properly implemented
+  };
+
   return (
     <div className='w-full h-screen overflow-x-hidden absolute'>
-      <div
-        className='md:ml-[18rem] md:mt-40 sm:mt-[11rem]
-       sm:ml-[5rem] sm:mr-[5rem] sm:mb-[11rem] h-full'
-      >
-        <section className={'flex flex-row justify-between w-full mb-14'}>
+      <div className='md:ml-[18rem] md:mt-40 md:mb-[11rem] tab:mx-[5rem] mx-[1.5rem] my-[9rem]'>
+        <section className='flex flex-col tab:flex-row tab:justify-between tab:items-center gap-10 tab:gap-1 w-full mb-6 tab:mb-14'>
           <h1
             className='col-span-2 min-[678px]:col-span-1 font-bold text-5xl bg-clip-text
-           text-transparent bg-gradient-to-r from-sky-500
-            to-indigo-500'
+            text-transparent bg-gradient-to-r from-sky-500 to-indigo-500'
           >
             Select Season
           </h1>
           <button
             onClick={() => navigate(``)} // TODO, create seasons page
             className={
-              'bg-green-500 hover:bg-green-700 h-fit disabled:bg-gray-500 text-white p-2 font-bold rounded-xl flex flex-row'
+              'bg-green-500 hover:bg-green-700 h-fit disabled:bg-gray-500 text-white p-2 rounded-xl flex justify-center align-center gap-1'
             }
           >
             <svg
@@ -78,7 +83,7 @@ const SeasonInstancesPage = () => {
               viewBox='0 0 24 24'
               strokeWidth={3}
               stroke='currentColor'
-              className='w-[2rem] h-[2rem] pr-1'
+              className='w-6 h-6'
             >
               <path
                 strokeLinecap='round'
@@ -86,10 +91,33 @@ const SeasonInstancesPage = () => {
                 d='M12 6v12m6-6H6'
               />
             </svg>
-            <span className={'my-auto text-lg'}>Create Season</span>
+            <p className='my-auto'>Add Season</p>
           </button>
         </section>
-        <ul className='md:grid md:grid-cols-2 md:gap-8 sm:grid sm:grid-cols-1 sm:gap-4 mt-9'>
+        <div className='mb-6'>
+          <label
+            htmlFor='event-select'
+            className='text-sm text-zinc-500 ml-1 mb-2 block'
+          >
+            Filter (Currently Unavailable)
+          </label>
+          <select
+            id='event-select'
+            className='select w-full tab:max-w-xs bg-white border border-zinc-300 rounded-lg p-3 text-zinc-600'
+            onChange={handleEventChange}
+          >
+            <option value='active' className='px-6 py-3'>
+              Active
+            </option>
+            <option value='inactive' className='px-6 py-3'>
+              Inactive
+            </option>
+            <option value='all' className='px-6 py-3'>
+              All
+            </option>
+          </select>
+        </div>
+        <ul className='md:grid md:grid-cols-2 md:gap-8 grid grid-cols-1 gap-4 mt-9'>
           {seasons.map((season) => (
             <li key={season.seasonid}>
               <button
