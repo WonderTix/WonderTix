fc763f26d25860fce8886b2e32db76f3fcfc5b4c
Hali Tran <78198010+HaliTran@users.noreply.github.com>
2023-10-15 15:43:27 -0700

    TIC-77: Abstract away season info to new component

--- client/src/components/Ticketing/ticketingmanager/Season/components/SeasonInfo.tsx
+++ client/src/components/Ticketing/ticketingmanager/Season/components/SeasonInfo.tsx
@@ -7,6 +7,7 @@ import {
   updateSeasonInfo,
   RequestBody,
 } from './utils/apiRequest';
+import ViewSeasonInfo from './utils/ViewSeasonInfo';
 
 interface SeasonProps {
   seasonId: number;
@@ -188,37 +189,7 @@ const SeasonInfo = (props: SeasonProps) => {
       </section>
     </form>
   ) : (
-    <header className='rounded-xl bg-white p-7 text-lg'>
-      <section className='flex flex-col text-center mb-5 justify-between tab:flex-row  '>
-        <h1 className='text-4xl mb-3 font-semibold'>Season Information</h1>
-        <button
-          className='bg-blue-500 hover:bg-blue-700 disabled:bg-gray-500 text-white font-bold py-2 px-10 rounded-xl'
-          onClick={() => setIsFormEditing(true)}
-        >
-          Edit
-        </button>
-      </section>
-
-      <section className='grid grid-cols-12'>
-        <article className='col-span-12 mb-5 text-center tab:text-start sm:col-span-6'>
-          <h3 className='font-semibold'>Season Name </h3>
-          <p className='mb-3 text-base'>{name}</p>
-
-          <h3 className='font-semibold'>Start Date </h3>
-          <p className='mb-3 text-base'>{startdate}</p>
-
-          <h3 className='font-semibold'>End Date </h3>
-          <p className='text-base'>{enddate}</p>
-        </article>
-        <article className='col-span-12 sm:col-span-6'>
-          <SeasonImage
-            className='h-auto max-w-[125px] mx-auto'
-            src={imageurl}
-            alt={`Cover photo for ${name} season`}
-          />
-        </article>
-      </section>
-    </header>
+    <ViewSeasonInfo {...seasonValues} setIsFormEditing={setIsFormEditing} />
   );
 };
 
