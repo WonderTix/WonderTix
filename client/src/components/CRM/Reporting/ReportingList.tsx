d6c51b7927c1bfd9480d2252161d4450bca58807
xiivi <jvn4@pdx.edu>
2023-10-15 16:26:45 -0700

    Refactored ReportingMain.tsx. Added ReportingList.tsx

---
+++ client/src/components/CRM/Reporting/ReportingList.tsx
@@ -0,0 +1,46 @@
+/* eslint-disable react/react-in-jsx-scope */
+import {Box} from '@mui/material';
+
+/**
+ * @returns {object} ReportingMain - has Navigation
+ *  and Reporting to reroute to other components
+ */
+const ReportingList = () => {
+  return (
+      <Box
+        sx={{
+          width: '100%',
+          height: '100%',
+          position: 'absolute',
+          overflowX: 'hidden',
+        }}
+      >
+        <Box
+          sx={{
+            width: '100%',
+            height: '100%',
+            marginLeft: '16.7rem',
+            marginRight: '4rem',
+          }}
+        >
+          <Box
+            sx={{
+              marginTop: '6rem',
+              marginBottom: '1.5rem',
+            }}
+          >
+            <h1 className='font-bold text-5xl bg-clip-text text-transparent bg-gradient-to-r from-violet-500 to-fuchsia-500'>
+              Reports
+            </h1>
+            <ul style={{listStyleType: 'disc', marginLeft: '1.5rem'}}>
+              <li><a href="/admin/reporting/daily-sales-report">Daily Sales Report</a></li>
+              <li><a href="/admin/reporting/donation-summary-report">Donation Summary Report</a></li>
+              <li><a href="/admin/reporting/credit-card-reconciliation">Credit Card Reconciliation</a></li>
+            </ul>
+          </Box>
+        </Box>
+      </Box>
+  );
+};
+
+export default ReportingList;
--- client/src/components/CRM/Reporting/ReportingMain.tsx
+++ client/src/components/CRM/Reporting/ReportingMain.tsx
@@ -1,8 +1,6 @@
 /* eslint-disable react/react-in-jsx-scope */
-import Reporting from './Reporting';
+import ReportingList from './ReportingList';
 import Navigation from '../Navigation';
-import CreditCardReconciliationReport from './CreditCardReconciliationReport/CreditCardReconciliationReport';
-import {Box} from '@mui/material';
 
 /**
  * @returns {object} ReportingMain - has Navigation
@@ -11,39 +9,7 @@ import {Box} from '@mui/material';
 const ReportingMain = () => {
   return (
     <div>
-      <Box
-        sx={{
-          width: '100%',
-          height: '100%',
-          position: 'absolute',
-          overflowX: 'hidden',
-        }}
-      >
-        <Box
-          sx={{
-            width: '100%',
-            height: '100%',
-            marginLeft: '16.7rem',
-            marginRight: '4rem',
-          }}
-        >
-          <Box
-            sx={{
-              marginTop: '6rem',
-              marginBottom: '1.5rem',
-            }}
-          >
-            <h1 className='font-bold text-5xl bg-clip-text text-transparent bg-gradient-to-r from-violet-500 to-fuchsia-500'>
-              Reports
-            </h1>
-            <ul style={{listStyleType: 'disc', marginLeft: '1.5rem'}}>
-              <li><a href="/admin/reporting/daily-sales-report">Daily Sales Report</a></li>
-              <li><a href="/admin/reporting/donation-summary-report">Donation Summary Report</a></li>
-              <li><a href="/admin/reporting/credit-card-reconciliation">Credit Card Reconciliation</a></li>
-            </ul>
-          </Box>
-        </Box>
-      </Box>
+      <ReportingList/>
       <Navigation/>
     </div>
   );
