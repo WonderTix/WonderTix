9eef5533e86ed56a9ad7bc7e7317ca3666b8bfde
Shihao126 <139028332+Shihao126@users.noreply.github.com>
2023-10-19 11:01:11 +0800

    Add files via upload
    Oct18/2023--version

---
+++ client/e2e/EventsPage.spec.ts
@@ -0,0 +1,39 @@
+import {test , expect} from '@playwright/test';
+import {EventsPage} from './pages/EventsPage';
+
+test('Homepage->Events',async({page}) => {
+  const eventsPage = new EventsPage(page);
+  await eventsPage.goto();
+});
+
+test('addNewEvents',async({page})=>{
+  const eventsPage = new EventsPage(page);
+  await eventsPage.goto();  
+  await eventsPage.addnewevent();
+});
+
+test('addDeleteEvents',async({page})=>{
+  const eventsPage = new EventsPage(page);
+  await eventsPage.goto();  
+  await eventsPage.addDeleteEvents();
+});
+
+test('editEvents',async({page})=>{
+  const eventsPage = new EventsPage(page);
+  await eventsPage.goto();
+  await eventsPage.editEvents();
+});
+
+test('editEvents1',async({page})=>{
+  const eventsPage = new EventsPage(page);
+  await eventsPage.goto();
+  await eventsPage.editEvents1();
+});
+
+test('editShowing',async({page})=>{
+  const eventsPage = new EventsPage(page);
+  await eventsPage.goto();
+  await eventsPage.editShowing();
+});
+
+
