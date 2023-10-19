ac96ed1a224103da607b53a7cdf81ae2a8238c31
Hali Tran <78198010+HaliTran@users.noreply.github.com>
2023-10-19 00:30:25 -0700

    TIC-77: Add buttons for each event card

--- client/src/components/Ticketing/ticketingmanager/Season/components/EventCard.tsx
+++ client/src/components/Ticketing/ticketingmanager/Season/components/EventCard.tsx
@@ -9,17 +9,27 @@ interface EventCardProps {
 const EventCard = (props: EventCardProps) => {
   const {name, imageurl} = props;
   return (
-    <div className='flex flex-col items-center bg-gray-200 rounded-xl border-8 border-solid border-zinc-300 mb-3 p-3 tab:flex-row'>
-      <div id='event-image-container' className='w-32'>
-        <img
-          className='h-auto w-full'
-          src={
-            imageurl === 'Default Event Image' ? getImageDefault() : imageurl
-          }
-          alt={`Cover photo for ${name} event`}
-        />
-      </div>
-      <h3 className='ml-5 text-3xl font-bold'>{name}</h3>
+    <div className='flex flex-col items-center bg-gray-200 rounded-xl border-8 border-solid border-zinc-300 mb-3 p-3 tab:flex-row tab:justify-between'>
+      <article className='flex flex-col items-center tab:flex-row'>
+        <div id='event-image-container' className='w-32'>
+          <img
+            className='h-auto w-full'
+            src={
+              imageurl === 'Default Event Image' ? getImageDefault() : imageurl
+            }
+            alt={`Cover photo for ${name} event`}
+          />
+        </div>
+        <h3 className='ml-5 text-3xl font-bold'>{name}</h3>
+      </article>
+      <article className='flex flex-col my-2'>
+        <button className='bg-blue-500 hover:bg-blue-700 disabled:bg-gray-500 text-white py-2 px-3 rounded-xl mb-2'>
+          Go to Event Page
+        </button>
+        <button className='bg-red-500 hover:bg-red-600 disabled:bg-gray-500 text-white py-2 px-3 rounded-xl'>
+          Remove Event
+        </button>
+      </article>
     </div>
   );
 };
