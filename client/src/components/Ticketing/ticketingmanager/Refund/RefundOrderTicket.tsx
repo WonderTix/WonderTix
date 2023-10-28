0c1e3aed7dd5aa7393a3db90378d6500b407afbb
jonathantnguyen <jonnytnguyen@gmail.com>
2023-10-27 19:52:23 -0700

    Revision Changes

--- client/src/components/Ticketing/ticketingmanager/Refund/RefundOrderTicket.tsx
+++ client/src/components/Ticketing/ticketingmanager/Refund/RefundOrderTicket.tsx
@@ -80,92 +80,82 @@ const Refund = () => {
       }
       <div className='w-full h-screen overflow-x-hidden absolute '>
         <div className='md:ml-[18rem] md:mt-40 md:mb-[11rem] tab:mx-[5rem] mx-[1.5rem] my-[9rem]'>
-          <div className='flex'>
-            <h1 className='font-bold text-5xl bg-clip-text text-transparent bg-gradient-to-r from-sky-500 to-indigo-500 mb-14'>
-              Refund Ticket
-            </h1>
-          </div>
+          <h1 className='font-bold text-5xl bg-clip-text text-transparent bg-gradient-to-r from-sky-500 to-indigo-500 mb-14'>
+            Refund Ticket
+          </h1>
           <div className='mb-4'>
-            <label id='email-value' className='grid'>
+            <div id='email-value' className='grid'>
               <div className='flex'>
-                <svg xmlns="http://www.w3.org/2000/svg" className="pointer-events-none h-5 w-5 absolute ml-6 mt-3" viewBox="0 0 20 20" fill="gray">
-                  <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                  <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+                <svg xmlns='http://www.w3.org/2000/svg' className='pointer-events-none h-5 w-5 absolute ml-6 mt-3' viewBox='0 0 20 20' fill='gray'>
+                  <path d='M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z' />
+                  <path d='M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z' />
                 </svg>
                 <input // search button
-                  type='text'
+                  type='email'
                   className='text-black border-2 border-gray rounded-l-full pl-12 w-80 py-2'
                   id='email-search-input'
                   placeholder='John_Smith@email.com'
                 />
-                <button id='email-search-input' type='button' className='px-0'>
-                  <div className='hover:bg-sky-500 w-20 rounded-r-full bg-indigo-500 hover:drop-shadow-md'>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-11 w-11 pl-6" viewBox="0 0 20 20" fill="white">
-                      <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+                <button id='email-search-button' type='button' className='px-0'>
+                  <div className='hover:bg-sky-500 w-20 rounded-r-full bg-indigo-500 hover:drop-shadow-md active:bg-sky-600'>
+                    <svg xmlns='http://www.w3.org/2000/svg' className='h-11 w-11 pl-6' viewBox='0 0 20 20' fill='white'>
+                      <path fill-rule='evenodd' d='M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z' clip-rule='evenodd' />
                     </svg>
                   </div>
                 </button>
               </div>
-            </label>
-          </div>
-          <div className='w-full min-w-min'>
-            <div className='grid grid-cols-5 gap-2 bg-gray-200 h-18 rounded-lg shadow-md px-2 mb-2 font-bold'>
-              <div className='row-start-1 justify-self-start py-2 col-span-1'>
-                  Name
-              </div>
-              <div className='row-start-1 justify-self-start py-2 col-span-1'>
-                <div className="border-l border-l-gray-500/50 pl-2">
-                  Date
-                </div>
-              </div>
-              <div className='row-start-1 justify-self-start py-2 col-span-1'>
-                <div className="border-l border-l-gray-500/50 pl-2">
-                  Showing(s)
-                </div>
-              </div>
-              <div className='row-start-1 justify-self-start py-2 col-span-1'>
-                <div className="border-l border-l-gray-500/50 pl-2">
-                  Total
-                </div>
-              </div>
-              <div className='row-start-1 justify-self-center py-2 col-span-1'></div>
             </div>
-            {mappedInstances.length === 0 ? (
-              <div className="text-center text-gray-600">No current results</div>
-            ) : (
-              mappedInstances.map((instance, index) => (
-                <div key={index} className='grid grid-cols-5 gap-2 bg-gray-200 rounded-lg shadow-md px-2 mb-2 hover:bg-gray-300'>
-                  <div className='row-start-1 justify-self-start pl-2 py-2 col-span-1'>
-                    {instance.name}
-                  </div>
-                  <div className='row-start-1 justify-self-start pl-2 py-2 col-span-1'>
-                    {instance.date}
-                  </div>
-                  <div className='row-start-1 justify-self-start pl-2 py-2 col-span-1'>
-                    {Array.isArray(instance.showings) ? (
-                      instance.showings.map((showing, showingIndex) => (
-                        <div key={showingIndex}>{showing}</div>
-                      ))
-                    ) : (
-                      <div>{instance.showings}</div>
-                    )}
-                  </div>
-                  <div className='row-start-1 justify-self-start pl-2 py-2 col-span-1'>
-                    {instance.price}
-                  </div>
-                  <div className='row-start-1 justify-self-start py-2 col-span-1'>
-                    <button
-                      onClick={async () => await onSubmit(instance.orderID)}
-                      type={'submit'}
-                      className='bg-red-700 hover:bg-red-800 text-white py-2 px-4 rounded'
-                    >
-                      Refund
-                    </button>
-                  </div>
-                </div>
-              ))
-            )}
           </div>
+          <table className='w-full min-w-min'>
+            <thead>
+              <tr className='grid grid-cols-5 gap-2 bg-gray-200 h-18 rounded-lg shadow-md px-2 mb-2 font-bold'>
+                <td className='row-start-1 justify-self-start py-2 col-span-1'>Name</td>
+                <td className='row-start-1 justify-self-start py-2 col-span-1'>Date</td>
+                <td className='row-start-1 justify-self-start py-2 col-span-1'>Showing(s)</td>
+                <td className='row-start-1 justify-self-start py-2 col-span-1'>Total</td>
+                <td className='row-start-1 justify-self-center py-2 col-span-1'></td>
+              </tr>
+            </thead>
+            <tbody>
+              {mappedInstances.length === 0 ? (
+                <tr className='text-center text-gray-600'>
+                  <td className='col-span-5'><p>No current results</p></td>
+                </tr>
+              ) : (
+                mappedInstances.map((instance, index) => (
+                  <tr key={index} className='grid grid-cols-5 gap-2 bg-gray-200 rounded-lg shadow-md px-2 mb-2 hover:bg-gray-300'>
+                    <td className='row-start-1 justify-self-start pl-2 py-2 col-span-1'>
+                      {instance.name}
+                    </td>
+                    <td className='row-start-1 justify-self-start pl-2 py-2 col-span-1'>
+                      {instance.date}
+                    </td>
+                    <td className='row-start-1 justify-self-start pl-2 py-2 col-span-1'>
+                      {Array.isArray(instance.showings) ? (
+                        instance.showings.map((showing, showingIndex) => (
+                          <div key={showingIndex}>{showing}</div>
+                        ))
+                      ) : (
+                        <div>{instance.showings}</div>
+                      )}
+                    </td>
+                    <td className='row-start-1 justify-self-start pl-2 py-2 col-span-1'>
+                      {instance.price}
+                    </td>
+                    <td className='row-start-1 justify-self-start py-2 col-span-1'>
+                      <button
+                        onClick={() => onSubmit(instance.orderID)}
+                        type={'submit'}
+                        className='bg-red-700 hover:bg-red-800 text-white py-2 px-4 rounded active:bg-red-900'
+                      >
+                        Refund
+                      </button>
+                    </td>
+                  </tr>
+                ))
+              )}
+            </tbody>
+          </table>
         </div>
       </div>
     </>
