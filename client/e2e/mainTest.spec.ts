6c6892aa538921df006dd18706d8f215fa73c9f8
snorq2 <81600811+snorq2@users.noreply.github.com>
2023-10-31 19:37:08 -0700

    DEP-73 Added event removal and error cleanup

--- client/e2e/mainTest.spec.ts
+++ client/e2e/mainTest.spec.ts
@@ -22,48 +22,35 @@ test('view show', async ({page}) => {
 // Go through all the steps to add a ticket
 // Captures returns needed to construct the confirmation message
 // Verifies the 'Success!' message pops up with the correct event info
-test('add ticket', async ({page}) => {
-  const main = new MainPage(page);
-  await main.goto();
-  const showing = await main.goFirstShowing();
-  const date = await main.selectRandomDate();
-  await main.selectRandomTime();
-  await main.selectRandomTicketType();
-  const quantity = await main.selectRandomQuantity();
-  const dateParts = date.split(' ');
-  let confirmMessage;
-  confirmMessage = 'You added ' + quantity + ' ticket';
-  if (parseInt(quantity) > 1) {
-    confirmMessage += 's';
-  }
-  confirmMessage += ' to ' + showing + ' on ' + dateParts[1] + ' ' + dateParts[2] + ' to the cart.';
-  await main.clickGetTickets();
-  expect(await main.checkAddTicketSucess(confirmMessage)).toBeTruthy();
-});
-
 // Updated version of add ticket test
-test('add ticket2', async ({page}) => {
+test('add ticket', async ({page}) => {
   const events = new EventsPage(page);
   const main = new MainPage(page);
   await events.goto();
   await events.addnewevent(EventsInfo2);
   await events.addNewShowing(ShowingInfo2);
-  await main.goto();
-  // await main.goSelectShowing('Test_eventAn event for testingSee Showings');
-  // Test_eventAn event for testingSee Showings
-  const showing = await main.goSelectShowing(EventsInfo2);
-//   const showing = await main.goFirstShowing();
-  const date = await main.selectRandomDate();
-  await main.selectRandomTime();
-  await main.selectRandomTicketType();
-  const quantity = await main.selectRandomQuantity();
-  const dateParts = date.split(' ');
-  let confirmMessage;
-  confirmMessage = 'You added ' + quantity + ' ticket';
-  if (parseInt(quantity) > 1) {
-    confirmMessage += 's';
+  try {
+    await main.goto();
+    const showing = await main.goSelectShowing(EventsInfo2);
+    expect(EventsInfo2.eventName).toEqual(showing);
+    const date = await main.selectRandomDate();
+    expect(ShowingInfo2.showingWholeDate).toEqual(date);
+    await main.selectRandomTime();
+    await main.selectRandomTicketType();
+    const quantity = await main.selectRandomQuantity();
+    const dateParts = date.split(' ');
+    let confirmMessage: string;
+    confirmMessage = 'You added ' + quantity + ' ticket';
+    if (parseInt(quantity) > 1) {
+      confirmMessage += 's';
+    }
+    confirmMessage += ' to ' + showing + ' on ' + dateParts[1] + ' ' + dateParts[2] + ' to the cart.';
+    await main.clickGetTickets();
+    expect(await main.checkAddTicketSucess(confirmMessage)).toBeTruthy();
+  } finally {
+    await main.goto();
+    await events.goToEventFromManage(EventsInfo2.eventFullName);
+    await events.searchDeleteShowing(ShowingInfo2.showingWholeDate);
+    await events.deleteTheEvent(EventsInfo2.eventFullName);
   }
-  confirmMessage += ' to ' + showing + ' on ' + dateParts[1] + ' ' + dateParts[2] + ' to the cart.';
-  await main.clickGetTickets();
-  expect(await main.checkAddTicketSucess(confirmMessage)).toBeTruthy();
 });
