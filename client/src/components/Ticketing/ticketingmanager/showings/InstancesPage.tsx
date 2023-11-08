253d562dc3dd7471911ab3b54377c61b7d9eeefb
jonathantnguyen <jonnytnguyen@gmail.com>
2023-11-08 11:50:14 -0800

    Removed unused function

--- client/src/components/Ticketing/ticketingmanager/showings/InstancesPage.tsx
+++ client/src/components/Ticketing/ticketingmanager/showings/InstancesPage.tsx
@@ -51,10 +51,6 @@ const InstancesPage = (): ReactElement => {
       });
   }, [filterSetting]);
 
-  const handleFilterChange = (newFilter) => {
-    setFilterSetting(newFilter);
-  };
-
   return (
     <div className='w-full h-screen overflow-x-hidden absolute'>
       <div className='md:ml-[18rem] md:mt-40 md:mb-[11rem] tab:mx-[5rem] mx-[1.5rem] my-[9rem]'>
