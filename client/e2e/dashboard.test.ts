0694ecc560e3a67e21775d0af71a18d1971436e6
Henry Kaus <92124260+henrykaus@users.noreply.github.com>
2023-11-07 21:48:46 -0800

    [TIC-118] Rename Refund Ticket to Refund Orders (#430)

--- client/e2e/dashboard.test.ts
+++ client/e2e/dashboard.test.ts
@@ -9,18 +9,18 @@ test('Homepage->Ticketing Dashboard', async ({page}) => {
   await expect(dashboard.page).toHaveURL('/ticketing');
 });
 
-test('Ticketing Dashboard->Door List', async ({page}) => {
+test('Ticketing Dashboard->DoorList', async ({page}) => {
   const dashboard = new DashboardPage(page);
 
   await dashboard.DoorList();
-  await dashboard.backtoDashboard();
+  await dashboard.backToDashboard();
 });
 
 test('Ticketing Dashboard->Events', async ({page}) => {
   const dashboard = new DashboardPage(page);
 
   await dashboard.Events();
-  await dashboard.backtoDashboard();
+  await dashboard.backToDashboard();
 });
 
 test('Ticketing Dashboard->PurchaseTickets', async ({page}) => {
@@ -30,11 +30,18 @@ test('Ticketing Dashboard->PurchaseTickets', async ({page}) => {
   await dashboard.ticketingURL();
 });
 
+test('Ticketing Dashboard->Seasons', async ({page}) => {
+  const dashboard = new DashboardPage(page);
+
+  await dashboard.Seasons();
+  await dashboard.ticketingURL();
+});
+
 test('Ticketing Dashboard->CreateNewsletter', async ({page}) => {
   const dashboard = new DashboardPage(page);
 
   await dashboard.CreateNewsletter();
-  await dashboard.backtoDashboard();
+  await dashboard.backToDashboard();
 });
 
 test('Ticketing Dashboard->ManageSeasonalTickets', async ({page}) => {
@@ -47,12 +54,19 @@ test('Ticketing Dashboard->ManageTicketTypes', async ({page}) => {
   const dashboard = new DashboardPage(page);
 
   await dashboard.ManageTicketTypes();
-  await dashboard.backtoDashboard();
+  await dashboard.backToDashboard();
 });
 
 test('Ticketing Dashboard->TicketExchanges', async ({page}) => {
   const dashboard = new DashboardPage(page);
 
   await dashboard.TicketExchanges();
-  await dashboard.backtoDashboard();
+  await dashboard.backToDashboard();
+});
+
+test('Ticketing Dashboard->RefundOrders', async ({page}) => {
+  const dashboard = new DashboardPage(page);
+
+  await dashboard.RefundOrders();
+  await dashboard.backToDashboard();
 });
