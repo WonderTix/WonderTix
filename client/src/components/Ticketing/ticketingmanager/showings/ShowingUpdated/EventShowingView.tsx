33f7aedb5d29c8228d69ad7a1731192a78e66eab
txiong002 <tfx22@yahoo.com>
2023-11-08 13:02:43 -0800

    TIC-78: working on implenting changing prices in events page

--- client/src/components/Ticketing/ticketingmanager/showings/ShowingUpdated/EventShowingView.tsx
+++ client/src/components/Ticketing/ticketingmanager/showings/ShowingUpdated/EventShowingView.tsx
@@ -1,5 +1,5 @@
 import {UpdatedShowing} from '../../../../../interfaces/showing.interface';
-import React from 'react';
+import React, {useState} from 'react';
 import format from 'date-fns/format';
 import {toDateStringFormat} from './util/EventsUtil';
 import {useEvent} from './EventProvider';
@@ -14,12 +14,22 @@ interface EventInstanceViewProps {
 
 export const EventShowingView = (props: EventInstanceViewProps) => {
   const {showing, setEdit} = props;
+
   const {ticketTypes, editing, showPopUp} = useEvent();
+  const [editPrices, setEditedPrices] = useState({});
+
   const showingDate = new Date(
     `${toDateStringFormat(showing.eventdate)} ${showing.eventtime
       .split('T')[1]
       .slice(0, 8)}`,
   );
+
+  const handlePriceChange = (typeID, price) => {
+    setEditedPrices((prevState) => ({
+      ...prevState,
+      [typeID]: price,
+    }));
+  };
 
   return (
     <div className={'bg-gray-300 rounded-xl p-2'}>
@@ -90,7 +100,10 @@ export const EventShowingView = (props: EventInstanceViewProps) => {
                       )}
                     </td>
                     <td className={'px-2'}>
-                      {getTicketTypePrice(type.typeID, 'price', ticketTypes)}
+                      <input
+                      type='string'
+                      value={editPrices[type.typeID] || getTicketTypePrice(type.typeID, 'price', ticketTypes)}
+                      onChange={(e) => handlePriceChange(type.typeID, parseFloat(e.target.value))} />
                     </td>
                     <td className={'px-2'}>
                       {getTicketTypePrice(
