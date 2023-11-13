80c3cdcbb83132090b277a9bae2d35655c9b3f3e
Ben Austin <bena2@pdx.edu>
2023-11-12 16:39:22 -0800

    TIC-136: fixed typo

--- client/src/components/Authentication/protected-route.tsx
+++ client/src/components/Authentication/protected-route.tsx
@@ -4,7 +4,6 @@ and only retrns it if one is logged in.
 */
 import {withAuthenticationRequired} from '@auth0/auth0-react';
 import React from 'react';
-import {boolean} from 'yup';
 /**
  * takes a component in as a prop,
 and only retrns it if one is logged in
@@ -23,8 +22,6 @@ const ProtectedRoute = ({component}: {component: any}): React.ReactElement => {
 export default ProtectedRoute;
 
 const claimCheck = (user): boolean => {
-  return user?.undefineduser_authorization?.permissions.includes(
-      (permission: string) => permission === 'admin',
-  );
+  return user?.undefineduser_authorization?.permissions.includes('admin');
 };
 
