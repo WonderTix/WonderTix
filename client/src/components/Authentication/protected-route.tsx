eff901da1dbd50bd1791aabb9142f30e16248112
Ben Austin <bena2@pdx.edu>
2023-11-12 16:28:58 -0800

    TIC-136: Updated protected route component to verify admin status

--- client/src/components/Authentication/protected-route.tsx
+++ client/src/components/Authentication/protected-route.tsx
@@ -4,6 +4,7 @@ and only retrns it if one is logged in.
 */
 import {withAuthenticationRequired} from '@auth0/auth0-react';
 import React from 'react';
+import {boolean} from 'yup';
 /**
  * takes a component in as a prop,
 and only retrns it if one is logged in
@@ -12,14 +13,18 @@ and only retrns it if one is logged in
  * @param root0.component
  * @returns {React.ReactElement}
  */
-const ProtectedRoute = ({
-  component,
-}: {
-  component: any;
-}): React.ReactElement => {
-  const Component = withAuthenticationRequired(component);
-
+const ProtectedRoute = ({component}: {component: any}): React.ReactElement => {
+  const Component = withAuthenticationRequired(
+    withAuthenticationRequired(component, {claimCheck, returnTo: '/'}),
+  );
   return <Component />;
 };
 
 export default ProtectedRoute;
+
+const claimCheck = (user): boolean => {
+  return user?.undefineduser_authorization?.permissions.includes(
+      (permission: string) => permission === 'admin',
+  );
+};
+
