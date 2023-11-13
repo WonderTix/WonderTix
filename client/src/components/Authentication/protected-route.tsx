3b53bd27c340f6db528ead15e713fdcd1942c436
Ben Austin <bena2@pdx.edu>
2023-11-12 22:36:52 -0800

    TIC-136: Updated protected route to require admin permissions

--- client/src/components/Authentication/protected-route.tsx
+++ client/src/components/Authentication/protected-route.tsx
@@ -1,25 +1,32 @@
 /*
 This component takes a component in as a prop,
-and only retrns it if one is logged in.
+and only returns it if one is logged in and an admin.
 */
-import {withAuthenticationRequired} from '@auth0/auth0-react';
+import {useAuth0, withAuthenticationRequired} from '@auth0/auth0-react';
 import React from 'react';
+import {LoadingScreen} from '../Ticketing/mainpage/LoadingScreen';
 /**
  * takes a component in as a prop,
-and only retrns it if one is logged in
+ and only retrns it if one is logged in
  *
  * @param root0
  * @param root0.component
  * @returns {React.ReactElement}
  */
-const ProtectedRoute = ({
-  component,
-}: {
-  component: any;
-}): React.ReactElement => {
-  const Component = withAuthenticationRequired(component);
-
+const ProtectedRoute = ({component}: {component: any}): React.ReactElement => {
+  const {isLoading} = useAuth0();
+  const Component = withAuthenticationRequired(
+      withAuthenticationRequired(component, {claimCheck, returnTo: '/'}),
+  );
+  if (isLoading) {
+    return <LoadingScreen />;
+  }
   return <Component />;
 };
 
 export default ProtectedRoute;
+
+const claimCheck = (user): boolean => {
+  return user?.undefineduser_authorization?.permissions.includes('admin');
+};
+
