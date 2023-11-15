bc11c8b7b1e264bf774fee8fbd3e31f3ca8cfb2d
Henry Kaus <92124260+henrykaus@users.noreply.github.com>
2023-11-15 15:07:50 -0800

    [TIC-119] Add requirements to Admin Purchase Form (#459)

--- client/src/components/Ticketing/ticketingmanager/AdminPurchase/AdminCheckout.tsx
+++ client/src/components/Ticketing/ticketingmanager/AdminPurchase/AdminCheckout.tsx
@@ -3,10 +3,10 @@
 import {
   removeAllTicketsFromCart,
   selectDiscount,
-} from '../../ticketingmanager/ticketing/ticketingSlice';
+} from '../ticketing/ticketingSlice';
 import {useAppDispatch, useAppSelector} from '../../app/hooks';
 import {loadStripe} from '@stripe/stripe-js';
-import {ReactElement, useState} from 'react';
+import {ReactElement} from 'react';
 import AdminCompleteOrderForm, {
   CheckoutFormInfo,
 } from './AdminCompleteOrderForm';
@@ -30,19 +30,16 @@ export default function AdminCheckout(): ReactElement {
   const cartItems = location.state?.cartItems || [];
   const discount = useAppSelector(selectDiscount);
 
-  const handleBackButton = () => {
-    navigate('/ticketing/purchaseticket');
-  };
-
   const doCheckout = async (formData: CheckoutFormInfo) => {
     try {
       if (formData.seatingAcc === 'Other') {
         formData.seatingAcc = formData.comments;
       }
-      const donation = formData.donation;
-      const stripe = await stripePromise;
+      const donation = +formData.donation;
 
+      const stripe = await stripePromise;
       if (!stripe) return;
+
       const response = await fetch(
         process.env.REACT_APP_API_2_URL + `/events/checkout`,
         {
@@ -79,8 +76,8 @@ export default function AdminCheckout(): ReactElement {
   return (
     <div className='w-full h-screen overflow-x-hidden absolute'>
       <div className='flex flex-col lg:ml-[15rem] lg:mx-[5rem] md:ml-[13rem] tab:mx-[2rem] mx-[0.5rem] mt=[5rem] mb-[9rem]'>
-        <div className='flex flex-col mt-[6rem] items-center md:flex-row rounded-[1rem] md:items-stretch md:bg-white sm:flex-col w-full h-full'>
-          <div className='min-w-414 sm:w-full h-full md:m-[2rem] sm:mt-10 bg-zinc-100 p-2 md:p-[1rem] flex flex-col gap-5 items-start rounded-xl overflow-auto'>
+        <div className='flex flex-col mt-[6rem] items-center md:flex-row rounded-[1rem] md:items-stretch md:bg-white w-full h-full'>
+          <div className='min-w-414 w-full h-full md:m-[2rem] mt-10 bg-zinc-100 p-2 md:p-[1rem] flex flex-col gap-5 items-start rounded-xl overflow-auto'>
             <div className='flex flex-col items-center h-auto w-full'>
               <div className='text-2xl lg:text-5xl font-bold mb-5'>
                 Complete Order
@@ -93,7 +90,7 @@ export default function AdminCheckout(): ReactElement {
             </div>
           </div>
           <div
-            className='md:w-[30rem] sm:w-full sm:mt-10
+            className='md:w-[30rem] w-full mt-10
                md:ml-5 md:m-[2rem] bg-zinc-900 p-9 flex
                 flex-col items-center rounded-xl justify-between'
           >
