9dbc5cd41e9fce53df64f0ceacf326661b77c9ef
Henry Kaus <92124260+henrykaus@users.noreply.github.com>
2023-11-19 17:58:58 -0800

    [TIC-144] Reduce PNG Image Sizes (#465)

--- client/src/utils/imageURLValidation.tsx
+++ client/src/utils/imageURLValidation.tsx
@@ -1,30 +1,31 @@
 import React from 'react';
-import defaultImage from '../assets/DefaultEventImage.png';
+import defaultEventImage from '../assets/default_event_image.png';
 
-export const getImageDefault = (url?:string) => {
+export const getImageDefault = (url?: string) => {
   if (url && url !== '' && url !== 'Default Event Image') {
     return url;
   }
-  return defaultImage;
+  return defaultEventImage;
 };
 
 export const imageOnError = (event) => {
-  event.currentTarget.src = defaultImage;
+  event.currentTarget.src = defaultEventImage;
 };
 
 interface EventImageProps {
-  className:string;
-  src:string;
-  title:string;
+  className: string;
+  src: string;
+  title: string;
 }
 
-export const EventImage = (props:EventImageProps) => {
+export const EventImage = (props: EventImageProps) => {
   const {src, className, title} = props;
   return (
     <img
       className={className}
       src={getImageDefault(src)}
       alt={`${title} Playbill`}
-      onError={imageOnError}/>
+      onError={imageOnError}
+    />
   );
 };
