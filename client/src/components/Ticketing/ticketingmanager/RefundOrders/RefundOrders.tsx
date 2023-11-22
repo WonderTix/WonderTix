0d415f254adc21e011a7b694d545841947d8e5a5
Henry Kaus <henrykaus19@gmail.com>
2023-11-21 22:12:31 -0800

    TIC-148: removed console.log

--- client/src/components/Ticketing/ticketingmanager/RefundOrders/RefundOrders.tsx
+++ client/src/components/Ticketing/ticketingmanager/RefundOrders/RefundOrders.tsx
@@ -113,7 +113,6 @@ const RefundOrders = () => {
         throw response;
       }
       const data = await response.json();
-      console.log(data);
       setOrders(data);
     } catch (error) {
       console.error(error);
