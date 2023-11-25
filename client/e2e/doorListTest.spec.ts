3e1f3e7f68d84607f52b29a45ec3f5e8a1a74e3b
liamm <131229216+l-c-mcc@users.noreply.github.com>
2023-11-25 15:50:07 -0800

    updated last test to use new EventsInfo

--- client/e2e/doorListTest.spec.ts
+++ client/e2e/doorListTest.spec.ts
@@ -1,7 +1,7 @@
 import {test, expect} from '@playwright/test';
 import {DoorListPage} from './pages/doorListPage';
 import {EventsPage} from './pages/EventsPage';
-import {EventsInfo2, ShowingInfo1} from './testData/ConstsPackage';
+import {EventsInfo, EventsInfoTemplate2, ShowingInfo1} from './testData/ConstsPackage';
 
 
 test('Check Home', async ({page}) => {
@@ -11,23 +11,24 @@ test('Check Home', async ({page}) => {
 });
 
 test('Select a Showing', async ({page}) => {
+  const event = new EventsInfo(EventsInfoTemplate2);
   try {
     //add event and showing to check for in the door list
     const eventsPage = new EventsPage(page);
     await eventsPage.goto();
-    await eventsPage.addnewevent(EventsInfo2);
+    await eventsPage.addnewevent(event);
     await eventsPage.activateEvent();
     await eventsPage.addNewShowing(ShowingInfo1);
 
     //check door list
     const doorList = new DoorListPage(page);
     await doorList.goto();
-    await doorList.searchShowing(EventsInfo2,ShowingInfo1);
+    await doorList.searchShowing(event,ShowingInfo1);
   } finally {
     //remove the added event
     const eventsPage2 = new EventsPage(page);
     await eventsPage2.goto();
-    await page.locator(':text("' + EventsInfo2.eventName + '")').click();
-    await eventsPage2.deleteTheEvent(EventsInfo2.eventFullName);
+    await page.locator(':text("' + event.eventName + '")').click();
+    await eventsPage2.deleteTheEvent(event.eventFullName);
   }
 });
