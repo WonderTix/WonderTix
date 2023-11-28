36e277bf3cc90efb4a697fe9c0dc02b0b27706e1
jonathantnguyen <jonnytnguyen@gmail.com>
2023-11-27 18:17:05 -0800

    removed edit button

--- client/src/components/Ticketing/ticketingmanager/Season/components/SeasonTicketViewTable.tsx
+++ client/src/components/Ticketing/ticketingmanager/Season/components/SeasonTicketViewTable.tsx
@@ -7,20 +7,14 @@ interface EventInstanceViewProps {
 
 export const SeasonTicketViewTable = () => {
   return (
-    <div className={'bg-gray-300 grid grid-cols-12 gap-2 rounded-xl p-2 pr-6 h-[100%]'}>
-      <div className={'overflow-y-auto overflow-x-auto col-span-12 lg:col-span-11 shadow-xl border border-white rounded-xl bg-white w-[100%] min-h-[100px]'}>
+    <div className={'bg-gray-300 grid grid-cols-12 rounded-xl p-2 h-[100%]'}>
+      <div className={'overflow-y-auto overflow-x-auto col-span-12 shadow-xl border border-white rounded-xl bg-white w-[100%] min-h-[100px]'}>
         <table className={'table table-fixed text-sm min-w-[100%]'}>
-          <thead className={`text-left text-zinc-800 whitespace-nowrap bg-gray-300 top-0 `}>
+          <thead className={'text-left text-zinc-800 whitespace-nowrap bg-gray-300 top-0'}>
             <tr>
-              <th className={'px-2 py-1 border border-white'}>
-                Admission Type
-              </th>
-              <th className={'px-2 py-1 border border-white'}>
-                Ticket Price
-              </th>
-              <th className={'px-2 py-1 border border-white'}>
-                Concession Price
-              </th>
+              <th className={'px-2 py-1 border border-white'}>Admission Type</th>
+              <th className={'px-2 py-1 border border-white'}>Ticket Price</th>
+              <th className={'px-2 py-1 border border-white'}>Concession Price</th>
               <th className={'px-2 py-1 border border-white'}>Quantity</th>
             </tr>
           </thead>
@@ -37,11 +31,6 @@ export const SeasonTicketViewTable = () => {
             </tr>
           </tbody>
         </table>
-      </div>
-      <div className={'grid content-center mx-auto col-span-12 lg:col-span-1 pr-2'}>
-        <button className={' bg-blue-500 hover:bg-blue-700 disabled:bg-gray-500 text-white font-bold p-2 px-4 rounded-xl'}>
-          Edit
-        </button>
       </div>
     </div>
   );
