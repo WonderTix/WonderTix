e70ac3a89afa85ac3aeccd71faf504c15e6da05e
jonathantnguyen <jonnytnguyen@gmail.com>
2023-11-27 18:57:53 -0800

    thinner border

--- client/src/components/Ticketing/ticketingmanager/Season/components/SeasonTicketViewTable.tsx
+++ client/src/components/Ticketing/ticketingmanager/Season/components/SeasonTicketViewTable.tsx
@@ -7,7 +7,7 @@ interface EventInstanceViewProps {
 
 export const SeasonTicketViewTable = () => {
   return (
-    <div className={'bg-gray-300 grid grid-cols-12 rounded-xl p-2 h-[100%]'}>
+    <div className={'bg-gray-300 grid grid-cols-12 rounded-xl p-1 h-[100%]'}>
       <div className={'overflow-y-auto overflow-x-auto col-span-12 shadow-xl border border-white rounded-xl bg-white w-[100%] min-h-[100px]'}>
         <table className={'table table-fixed text-sm min-w-[100%]'}>
           <thead className={'text-left text-zinc-800 whitespace-nowrap bg-gray-300 top-0'}>
