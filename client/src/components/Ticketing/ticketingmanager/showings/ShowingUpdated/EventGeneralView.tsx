ceb0a09cbbd42225ae3c824fdfe5d10859e268fd
Henry Kaus <henrykaus19@gmail.com>
2023-11-30 20:22:02 -0800

    TIC-130: Removed overflow from buttons and made colors of titles consistent with their buttons

--- client/src/components/Ticketing/ticketingmanager/showings/ShowingUpdated/EventGeneralView.tsx
+++ client/src/components/Ticketing/ticketingmanager/showings/ShowingUpdated/EventGeneralView.tsx
@@ -39,20 +39,20 @@ export const EventGeneralView = (props: EventGeneralViewProps) => {
     <div className={'bg-white flex flex-col p-6 rounded-xl shadow-xl'}>
       <div
         className={
-          'flex flex-col gap-3 text-center mb-5 justify-between tab:flex-row tab:flex-wrap'
+          'flex flex-col gap-3 text-center mb-5 justify-between min-[650px]:flex-row min-[650px]:flex-wrap'
         }
       >
         <h2 className={'text-3xl font-semibold text-zinc-800'}>
           Event Information
         </h2>
-        <div className='flex flex-col gap-2 tab:flex-row tab:flex-wrap'>
-          <span
+        <div className='flex flex-col gap-2 min-[650px]:flex-row'>
+          <p
             className={`${
               eventData.active ? 'bg-green-100' : 'bg-red-100'
-            } py-2 px-8 rounded-lg font-medium`}
+            } py-2 tab:px-2 md:px-7 rounded-lg font-medium`}
           >
             {eventData.active ? 'ACTIVE' : 'INACTIVE'}
-          </span>
+          </p>
 
           <Tooltip title='Edit' placement='top' arrow>
             <span className='flex flex-col'>
