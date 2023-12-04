962f2884342fb92645d0ccdfb19f38cad2ecc7bd
Henry Kaus <henrykaus19@gmail.com>
2023-12-03 19:36:05 -0800

    Refactored CheckoutSuccess for accessibility and mobile improvements

--- client/src/components/Ticketing/checkout/CheckoutSuccess.tsx
+++ client/src/components/Ticketing/checkout/CheckoutSuccess.tsx
@@ -1,56 +1,40 @@
-/**
- * Copyright © 2021 Aditya Sharoff, Gregory Hairfeld, Jesse Coyle, Francis Phan, William Papsco, Jack Sherman, Geoffrey Corvera
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
 import React, {ReactElement} from 'react';
 import {useNavigate} from 'react-router-dom';
-
-interface CheckoutSuccessProp {
-  completeOrderButton: string;
-}
 
 /**
  * Displays the CheckoutSuccess page
  *
- * @param CheckoutSuccessProp
- * @param CheckoutSuccessProp.completeOrderButton
- * @returns {ReactElement}
+ * @returns {ReactElement} CheckoutSuccess
  */
 export default function CheckoutSuccess(): ReactElement {
   const navigate = useNavigate();
 
   return (
-    <div className='w-full h-screen items-center flex flex-row bg-zinc-100'>
-      <div className='w-full flex flex-col items-center text-2xl font-bold text-zinc-700 '>
-        <div className='flex flex-col items-center gap-5 bg-white p-7 rounded-xl shadow-xl'>
-          <div className='flex flex-row items-center '>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='h-12 w-12'
-              viewBox='0 0 20 20'
-              fill='currentColor'
-            >
-              <path
-                fillRule='evenodd'
-                d='M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
-                clipRule='evenodd'
-              />
-            </svg>
-            Thank you for your purchase!
-          </div>
-          <button
-            onClick={() => navigate('/')}
-            className='bg-blue-500 px-5 py-3 text-white text-lg font-normal rounded-xl hover:bg-blue-600'
+    <div className='w-full h-screen flex items-center justify-center font-bold bg-zinc-200'>
+      <section className='flex flex-col items-center gap-5 bg-zinc-100 p-7 rounded-lg shadow-xl mx-3'>
+        <h1 className='flex flex-col items-center text-center text-2xl text-zinc-800 tab:flex-row tab:text-left gap-x-2 gap-y-1'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-12 w-12 tab:h-10 tab:w-10'
+            viewBox='0 0 20 20'
+            fill='currentColor'
+            aria-hidden='true'
           >
-            Back to Home
-          </button>
-        </div>
-      </div>
+            <path
+              fillRule='evenodd'
+              d='M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
+              clipRule='evenodd'
+            />
+          </svg>
+          Thank you for your purchase!
+        </h1>
+        <button
+          onClick={() => navigate('/')}
+          className='bg-blue-500 px-5 py-3 text-white font-medium rounded-full hover:bg-blue-600'
+        >
+          Back to Home
+        </button>
+      </section>
     </div>
   );
 }
