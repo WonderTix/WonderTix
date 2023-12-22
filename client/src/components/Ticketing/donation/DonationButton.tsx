df881e1623e7efdf94271cb11028179c7e0e0054
Justin Greever <jgreever@gmail.com>
2023-12-21 23:48:21 -0800

    Don 154/donation webhook usage refactor (#517)
    Co-authored-by: miles <miles4599@gmail.com>
    Co-authored-by: 2miles <35203283+2miles@users.noreply.github.com>
    Co-authored-by: Dylan H <dstad41@hotmail.com>
    Co-authored-by: Ben Austin <bena2@pdx.edu>
    Co-authored-by: Ben Williams <ben16williams@gmail.com>
    Co-authored-by: ben16williams <55893544+ben16williams@users.noreply.github.com>

---
+++ client/src/components/Ticketing/donation/DonationButton.tsx
@@ -0,0 +1,27 @@
+import React, {ReactElement} from 'react';
+
+interface DonationButtonProps {
+  amount: number;
+  label: string;
+  onClick; // set the donation amount
+}
+
+/**
+ * Returns an individual button for the donation amount button array
+ * @param {DonationButtonProps}
+ * @returns {ReactElement}
+ */
+export default function DonationButton({
+  amount,
+  label,
+  onClick,
+}: DonationButtonProps): ReactElement {
+  return (
+    <button
+      className='bg-indigo-600 hover:bg-indigo-700 active:bg-transparent active:text-indigo-600 border border-indigo-600 text-white font-bold py-4 px-4 rounded'
+      onClick={() => onClick(amount)}
+    >
+      $ {amount} {label}
+    </button>
+  );
+}
