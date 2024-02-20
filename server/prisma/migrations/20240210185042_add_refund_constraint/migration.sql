e885d4f45cf6f6754439143d6178fdf99c65a6f4
Henry Kaus <henrykaus19@gmail.com>
2024-02-20 14:03:38 -0800

    WT-568: Removed random migrations

--- server/prisma/migrations/20240210185042_add_refund_constraint/migration.sql
+++
@@ -1,2 +0,0 @@
--- This is an empty migration.
-ALTER TABLE "refunditems" ADD CONSTRAINT "refund_ticketitems_check" CHECK (("donationid_fk" is null AND "order_ticketitemid_fk" is not null) OR ("order_ticketitemid_fk" is null AND "donationid_fk" is not null));
