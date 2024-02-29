80a2c8775cdcb8d43b3a36eefbd4c39e71aeb7fe
l-c-mcc <131229216+l-c-mcc@users.noreply.github.com>
2024-02-28 23:53:27 -0800

    playwright fix

--- client/e2e/global.setup.ts
+++ client/e2e/global.setup.ts
@@ -25,6 +25,7 @@ setup('authenticate', async ({page}) => {
 
   await loginPage.login(email, password);
 
+  await page.reload();
   // Edge case for "Wondertix App is trying to access your wtix-xxx account"
   try {
     // Wait for the Accept button to appear on the page.
