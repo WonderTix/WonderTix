397e16891bffb031cbe3a8ec50b52706ba02ea13
Deacon-Coding <cdeak42@gmail.com>
2024-03-04 12:38:01 -0800

    changed validateContact formData

--- server/src/controllers/eventController.service.ts
+++ server/src/controllers/eventController.service.ts
@@ -311,7 +311,7 @@ const validateContact = (formData: checkoutForm) => {
           new RegExp('^(\\+?\\d{1,2}\\s?)?\\(?\\d{3}\\)?[\\s.-]?\\d{3}[\\s.-]?\\d{4}$'),
       ),
     }),
-    ...(formData.visitSource && {visitSource: formData.visitSource}),
+    ...(formData.visitSource && {visitsource: formData.visitSource}),
     ...(formData.seatingAcc && {seatingaccom: formData.seatingAcc}),
     ...(formData.comments && {comments: formData.comments}),
     ...(formData.optIn && {newsletter: formData.optIn}),
