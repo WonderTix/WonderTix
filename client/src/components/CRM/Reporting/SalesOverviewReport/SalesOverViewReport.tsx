487de0e88d55db088d37f568be2fb2eba6e6857f
Kevin Vuong <kvuong@pdx.edu>
2024-03-04 17:38:18 -0800

    Adding filter setting to the Sales Overview Report

--- client/src/components/CRM/Reporting/SalesOverviewReport/SalesOverViewReport.tsx
+++ client/src/components/CRM/Reporting/SalesOverviewReport/SalesOverViewReport.tsx
@@ -1,6 +1,25 @@
 import React, {useState} from 'react';
+import FilterReports from './FilterReports/FilterReports';
 
 const DailySalesReport = (): React.ReactElement => {
+  const [showTools, setShowTools] = useState(false);
+  const [showFilter, setShowFilter] = useState(true);
+  const [selectedEndDate, setSelectedEndDate] = useState('');
+  const [selectedBeginDate, setSelectedBeginDate] = useState('');
+  const [showScheduledReports, setShowScheduledReports] = useState(false);
+
+
+  const handleGenerateClick = (beginDate: string, endDate: string): void => {
+    setSelectedBeginDate(beginDate);
+    setSelectedEndDate(endDate);
+    setShowTools(true);
+    setShowFilter(false);
+  };
+
+  const toggleScheduledReports = () => {
+    setShowScheduledReports(!showScheduledReports);
+  };
+
     return (
       <div className='w-full h-screen overflow-x-hidden absolute'>
         <div className='sm:my-32 sm:mx-4 md:my-32 md:ml-60 md:mr-14 lg:ml-64'>
@@ -9,6 +28,13 @@ const DailySalesReport = (): React.ReactElement => {
               bg-gradient-to-r from-violet-500 to-fuchsia-500 mb-14'>
               Sales Overview Report
             </h1>
+          </div>
+          <div className='flex md:flex-row md:items-start sm:flex-col sm:items-center justify-start'>
+            {showFilter && (
+              <FilterReports
+                onGenerateClick={handleGenerateClick}
+              />
+            )}
           </div>
         </div>
       </div>
