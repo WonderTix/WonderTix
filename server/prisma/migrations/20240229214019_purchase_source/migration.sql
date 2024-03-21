226f7fef26f7dc174252f9ce2fefdc1a0f9e5825
l-c-mcc <131229216+l-c-mcc@users.noreply.github.com>
2024-03-21 13:29:23 -0700

    modified migrations to allow for cleaner merging

--- server/prisma/migrations/20240229214019_purchase_source/migration.sql
+++ server/prisma/migrations/20240229214019_purchase_source/migration.sql
@@ -1,9 +1,6 @@
 -- CreateEnum
 CREATE TYPE "purchase_source" AS ENUM ('online_ticketing', 'admin_ticketing', 'card_reader', 'online_donation');
 
--- AlterEnum
-ALTER TYPE "state" ADD VALUE 'failed';
-
 -- AlterTable
 ALTER TABLE "orders" ADD COLUMN     "order_source" "purchase_source",
-ADD COLUMN     "order_status" "state";
+ADD COLUMN     "order_status" "state" DEFAULT NULL;
