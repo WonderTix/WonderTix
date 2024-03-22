beec721831b68446395f21bcea790f305453fcc9
l-c-mcc <131229216+l-c-mcc@users.noreply.github.com>
2024-03-22 14:50:21 -0700

    slight prisma correction

--- server/prisma/migrations/20240229214019_purchase_source/migration.sql
+++ server/prisma/migrations/20240229214019_purchase_source/migration.sql
@@ -3,4 +3,4 @@ CREATE TYPE "purchase_source" AS ENUM ('online_ticketing', 'admin_ticketing', 'c
 
 -- AlterTable
 ALTER TABLE "orders" ADD COLUMN     "order_source" "purchase_source",
-ADD COLUMN     "order_status" "state" DEFAULT NULL;
+ADD COLUMN     "order_status" "state";
