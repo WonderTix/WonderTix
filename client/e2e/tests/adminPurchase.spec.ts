53179b800e0af60a7fe20c35a560446d016b31e1
Henry Kaus <henrykaus19@gmail.com>
2024-04-29 12:54:20 -0700

    WT-649: test fix

--- client/e2e/tests/adminPurchase.spec.ts
+++ client/e2e/tests/adminPurchase.spec.ts
@@ -3,7 +3,7 @@ import {EventsPage} from '../pages/eventsPage';
 import {AdminPurchasePage} from '../pages/adminPurchasePage';
 import {EVENT_INFO_1} from '../testData/EventInfo';
 import {SHOWING_INFO_2} from '../testData/ShowingInfo';
-import {JANE_DOE} from '../testData/CustomerInfo';
+import {CustomerInfo, JANE_DOE} from '../testData/CustomerInfo';
 import {VALID_VISA_CREDIT} from '../testData/CreditCard';
 
 test('Purchase ticket for customer as admin', async ({page}, testInfo) => {
@@ -12,6 +12,7 @@ test('Purchase ticket for customer as admin', async ({page}, testInfo) => {
 
   const adminPage = new AdminPurchasePage(page);
   const events = new EventsPage(page);
+  const customer = new CustomerInfo(JANE_DOE);
   try {
     // Add event and showing
     await events.goTo();
@@ -21,7 +22,7 @@ test('Purchase ticket for customer as admin', async ({page}, testInfo) => {
 
     // Purchase ticket
     await adminPage.goTo();
-    await adminPage.purchaseTicket(EVENT_INFO_1.eventName, SHOWING_INFO_2.showingWholeDate, VALID_VISA_CREDIT, JANE_DOE);
+    await adminPage.purchaseTicket(EVENT_INFO_1.eventName, SHOWING_INFO_2.showingWholeDate, VALID_VISA_CREDIT, customer);
     await expect(adminPage.purchaseSuccessfulScreen).toBeVisible({timeout: 15000 + timeoutAdd});
   } finally {
     // Remove added event
