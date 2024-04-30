1b275220ea1e5933f6219af8a3ea09f4047126a7
Nate Ruble <rublenathaniel@gmail.com>
2024-04-30 00:16:08 -0700

    Created function to automatically generate navbuttons from list of objects

--- client/src/components/CRM/Navigation.tsx
+++ client/src/components/CRM/Navigation.tsx
@@ -36,45 +36,95 @@ const Navigation = (): React.ReactElement => {
 
   useOutsideClick(dropdownRef, () => setProfile(false));
 
-  const NavSymbol = (props) => {
+  const NavButton = (props) => {
     const [name] = useState(props.name);
     const [url] = useState(props.url);
     const [symbol] = useState(props.symbol);
     return (
       <li className="cursor-pointer mt-[12rem] text-zinc-200 font-semibold  text-md leading-3 tracking-normal  py-2 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none">
-              <a
-                onClick={() => navigate(url)}
-                className="bg-transparent border-none   rounded-none hover:scale-110 transition duration-300 ease-in-out"
+        <a
+          onClick={() => navigate(url)}
+          className="bg-transparent border-none   rounded-none hover:scale-110 transition duration-300 ease-in-out"
+        >
+          <div className="flex flex-col gap-2 items-center">
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-10 w-10"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
               >
-                <div className="flex flex-col gap-2 items-center">
-                  <div>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-10 w-10"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d={symbol}
-                      />
-                    </svg>
-                  </div>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d={symbol}
+                />
+              </svg>
+            </div>
 
-                  <span>{name}</span>
-                </div>
-              </a>
-            </li>
+            <span>{name}</span>
+          </div>
+        </a>
+      </li>
     );
   };
 
-  NavSymbol.propTypes = {
-    name: PropTypes.string.isRequired,
-    url: PropTypes.string.isRequired,
-    symbol: PropTypes.string.isRequired,
+  NavButton.propTypes = {
+    name: PropTypes.string,
+    url: PropTypes.string,
+    symbol: PropTypes.string,
+  };
+
+  const navButtons = [
+    {
+      name: 'Dashboard',
+      url: '/admin',
+      symbol: 'M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z',
+    },
+    {
+      name: 'Accounts',
+      url: '/admin/accounts',
+      symbol: 'M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z',
+    },
+    {
+      name: 'Contacts',
+      url: '/admin/contacts',
+      symbol: 'M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2',
+    },
+    {
+      name: 'Reporting',
+      url: '/admin/reporting',
+      symbol: 'M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z',
+    },
+    {
+      name: 'Task',
+      url: '/admin/tasks',
+      symbol: 'M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4',
+    },
+  ];
+
+  const NavSymbols = ({navButtons}) => {
+    return (
+      <ul className="space-y-7 mt-30 ml-[rem] flex flex-col items-center mb-7">
+        {
+            navButtons.map((values) => {
+              return React.createElement(NavButton, {
+                  name: values.name,
+                  url: values.url,
+                  symbol: values.symbol,
+                },
+              );
+            },
+          )
+        }
+      </ul>
+    );
+  };
+
+  NavSymbols.propTypes = {
+    navButtons: PropTypes.array,
   };
 
 
@@ -90,33 +140,7 @@ const Navigation = (): React.ReactElement => {
               <img src={logo} className="mt-[5rem] w-[6rem]" alt="/" />
             </button>
           </div>
-          <ul className="space-y-7 mt-30 ml-[rem] flex flex-col items-center mb-7">
-            <NavSymbol
-              name='Dashboard'
-              url='/admin'
-              symbol='M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z'
-            />
-            <NavSymbol
-              name='Accounts'
-              url='/admin/accounts'
-              symbol='M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z'
-            />
-            <NavSymbol
-              name='Contacts'
-              url='/admin/contacts'
-              symbol='M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2'
-            />
-            <NavSymbol
-              name='Reporting'
-              url='/admin/reporting'
-              symbol='M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z'
-            />
-            <NavSymbol
-                    name='Task'
-                    url='/admin/tasks'
-                    symbol='M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4'
-            />
-          </ul>
+          <NavSymbols navButtons={navButtons}/>
         </div>
 
 
@@ -168,33 +192,7 @@ const Navigation = (): React.ReactElement => {
                     </svg>
                   </button>
                 </div>
-                <ul className="space-y-7 flex flex-col items-center mb-7">
-                  <NavSymbol
-                    name='Dashboard'
-                    url='/admin'
-                    symbol='M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z'
-                  />
-                  <NavSymbol
-                    name='Accounts'
-                    url='/admin/accounts'
-                    symbol='M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z'
-                  />
-                  <NavSymbol
-                    name='Contacts'
-                    url='/admin/contacts'
-                    symbol='M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2'
-                  />
-                  <NavSymbol
-                    name='Reporting'
-                    url='/admin/reporting'
-                    symbol='M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z'
-                  />
-                  <NavSymbol
-                    name='Task'
-                    url='/admin/tasks'
-                    symbol='M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4'
-                  />
-                </ul>
+                  <NavSymbols navButtons={navButtons}/>
               </div>
 
               {/* Bottom corner nav elements */}
