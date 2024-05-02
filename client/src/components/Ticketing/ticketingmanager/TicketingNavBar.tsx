a098a287aa638b301c30b6182b5e451098c6eaac
Nate Ruble <rublenathaniel@gmail.com>
2024-05-01 18:49:59 -0700

    Added TicketingNavBar.tsx

---
+++ client/src/components/Ticketing/ticketingmanager/TicketingNavBar.tsx
@@ -0,0 +1,32 @@
+import React, {ReactElement} from 'react';
+import Navigation from '../Navigation';
+
+/**
+ * @returns {ReactElement} Dashmain - has Navigation
+ *  and Dashboard to reroute to other components
+ */
+const TicketingNavBar = (): ReactElement => {
+    const navButtons = [
+        {
+          name: 'Dashboard',
+          url: '/ticketing',
+          symbol: 'M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z',
+        },
+        {
+          name: 'Door List',
+          url: '/ticketing/doorlist',
+          symbol: 'M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z',
+        },
+        {
+          name: 'Events',
+          url: '/ticketing/events',
+          symbol: 'M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z',
+        },
+    ];
+
+    return (
+        <Navigation buttons={navButtons}/>
+    );
+  };
+
+  export default TicketingNavBar;
