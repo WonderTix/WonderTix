bc42f2894b17ebf4424613b2b792bb9d4638e3cd
Zach Roth <109639906+zroygbiv@users.noreply.github.com>
2024-05-21 14:30:16 -0700

    Wt-607: Init Terraform impl & documentation (#673)
    Co-authored-by: Omar Nassar <omariio1000@gmail.com>

---
+++ iac/terraform/env/prd/prd.tfvars
@@ -0,0 +1,3 @@
+database_version = "POSTGRES_15"
+region           = "us-west1"
+prd_tier         = "db-f1-micro"
---
+++ iac/terraform/output.tf
@@ -0,0 +1,5 @@
+# Output the Google Cloud project ID
+output "project_id" {
+  value       = "wondertix-app"
+  description = "The project ID used for all resources in this Terraform project."
+}
