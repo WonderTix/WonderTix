252d6780ce2198818ea3d9d68bd0846ec8700dca
Henry Kaus <92124260+henrykaus@users.noreply.github.com>
2024-06-23 21:04:41 -0700

    [WT-541] Added Search by Name to Refunds Page (#687)

--- client/src/components/Ticketing/Icons.tsx
+++ client/src/components/Ticketing/Icons.tsx
@@ -507,6 +507,27 @@ const TrashCanIcon = (props: IconProps) => {
   );
 };
 
+const UserIcon = (props: IconProps) => {
+  const {className = 'h-5 w-5', strokeWidth = 2} = props;
+
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      className={className}
+      fill='none'
+      viewBox='0 0 24 24'
+      stroke='currentColor'
+      strokeWidth={strokeWidth}
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z'
+      />
+    </svg>
+  );
+};
+
 const VerticalDotsIcon = (props: IconProps) => {
   const {className = 'h-5 w-5', strokeWidth = 2} = props;
   return (
@@ -576,6 +597,7 @@ export {
   SmallMinusIcon,
   TicketIcon,
   TrashCanIcon,
+  UserIcon,
   VerticalDotsIcon,
   XIcon,
 };
